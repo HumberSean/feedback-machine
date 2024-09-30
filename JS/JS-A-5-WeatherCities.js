e561e7c759f4889423830e0eca0884b52548a865
Sean Doyle <sean.doyle@humber.ca>
2024-09-30 11:11:42 -0400

    Add JS-A-4 and 5

---
+++ JS/JS-A-5-WeatherCities.js
@@ -0,0 +1,16 @@
+//JavaScript Assignment 5 - Weather Data for Two Cities
+
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback."];
+var item1=["PROFESSIONAL PRACTICE", "Add helpful comments to mark the sections of your code and significant logic pieces. I encourage you to do this first (&quot;pseudocode&quot;) before you start your work.", "Missing responses to professional practice questions."];
+var item2=["CLICKING A CITY", "Does not output the required city name (#3.a).", "The second button is missing a city name (#1).", "When the page first loads, the output panel should be hidden (#2).", "No data is provided for a second city."];
+var item3=["WEATHER ICON", "Missing the weather icon (#3.d).", "Missing alt attribute.", "alt text from a data object property - nice!"];
+var item4=["FIFTH DATA ITEM", "Missing contextual data (#3.e).", "Missing units of measure.", "Humidity should have %.", "Missing the required 5th data item (#3.e)."];
+var item5=["ERROR HANDLING", "Error message should be on its own, not mixed with original text.", "Does not let the user know there was an xhr request error.", "Be sure to test your error handling. You are not displaying an error message."];
+var item6=["STRETCH GOAL", "Well done!", "Your click listeners are calling anonymous functions."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6);
