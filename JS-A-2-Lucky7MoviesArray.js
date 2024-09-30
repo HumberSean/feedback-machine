be77529f17dbf536f4ed7bb46e9c396a6d2cc399
Sean Doyle <sean.doyle@humber.ca>
2024-09-30 10:57:59 -0400

    Add JS A-2

---
+++ JS-A-2-Lucky7MoviesArray.js
@@ -0,0 +1,16 @@
+//JavaScript Assignment 2 - Lucky 7 Movies Array
+//
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Put all JavaScript in the required external file (#1).", "You renamed your js file so I had to rename it back to match the HTML. Be sure to confirm your file submission in the folder before submitting."];
+var item1=["VARIABLES", "Create 7 variables for your movies (#2).", "Giving your variables more neutral names will make them future-proof. E.g. movie1, movie2..."];
+var item2=["ARRAY", "Use your variables in your array. No need to repeat the strings again. That's what the variables are for (#3)."];
+var item3=["RESULT MESSAGE", "Use your user input variable as the index to access your array... var movieChoice = myMovies[userInput - 1];", "Does not handle cancel (null).", "Does not handle 0.", "Popup should only show selected movie (#7).", "Movie number is off by one."];
+var item4=["CONSOLE OUTPUT", "The output to the console should have been a loop, since this lab was about arrays and loops. A for loop would be the correct loop to output from an array.", "Movie numbers in console output should start at 1, not 0.", "Message should include &quot;Movie&quot; and # in output text (#8).", "Starting your loop at 1 misses the first item in the array [0]", "Console output missing (#8)."];
+var item5=["STRETCH GOAL", "And the extra challenge too! Nice work!", "You need a variable to hold the prompt value inside your while statement. Use this to end your while loop.", "Avoid while(true) and try to create “self-commenting” code with variables.", "Be explicit in your while statement: (while inputValue === true)", "No need to check for true after the while loop. Logically, we can't get there until that condition is reached by the while loop."];
+var item6=["PROFESSIONAL PRACTICE", "Add helpful comments to mark the sections of your code and significant logic pieces. I encourage you to do this first (&quot;pseudocode&quot;) before you start your work.", "Missing responses to professional practice questions. Please respond to them in future assignments.", "Please email me if you are having trouble with this. We can make time to get you caught up. Each week will build on the previous week, so do not let yourself fall behind."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6);
