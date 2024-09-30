633613b35abe9c286e78b83fb231fd6d465c70f6
Sean Doyle <sean.doyle@humber.ca>
2024-09-30 11:02:55 -0400

    Add JS-A-3

---
+++ JS/JS-A-3-CustomObject.js
@@ -0,0 +1,14 @@
+//JavaScript Assignment 3 - Custom Object
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback."];
+var item1=["PROPERTIES", "Not all of these properties are meaningful to this object in a web application context (#3).", "The HTML file has not been updated to reflect your custom object."];
+var item2=["METHOD", "Your method (function) does not update the object, it only outputs the property values (#4).", "Your method should accept a parameter, rather than using a hard-coded value.", "The output (alert) should be in the function (# 4)."];
+var item3=["OBJECT INTERACTION", "You have output object properties, but you did not output the object itself to the console: console.log(myObject);(#6,#10)", "Output the object before and after your logic (#6,#10).", "Output the object before you call any methods so that we can see what it is when the page first loads.", "Popups to get user input should be prompt()", "Requirements are to update two properties with prompts, then call method directly to modify third property (#7,8,9). Please pay careful attention to requirements."];
+var item4=["STRETCH GOAL", "And the extra challenge too! Nice work!", "No need to separate extra challenge code, just give me the final version. Thank you!"];
+var item5=["PROFESSIONAL PRACTICE", "Add helpful comments to mark the sections of your code and significant logic pieces. I encourage you to do this first (&quot;pseudocode&quot;) before you start your work.", "Missing responses to professional practice questions. Please respond to them in future assignments.", "Please email me if you are having trouble with this. We can make time to get you caught up. Understanding objects is critical in JavaScript."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5);
