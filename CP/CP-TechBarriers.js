8a634b65884e483eae0b4adafe0557a6c11a4a7c
Sean Doyle <sean.doyle@humber.ca>
2024-10-01 09:52:58 -0400

    New file content

--- CP/CP-TechBarriers.js
+++ CP/CP-TechBarriers.js
@@ -1,13 +1,13 @@
 //CAPSTONE-TECHNICAL BARRIERS ASSESSMENT
 
 
+
 var feedBack = [];
 
-var item0=["CONTENT CHALLENGES", "How will you mitigate this challenge?", "What content challenges do you anticipate?", "Are you comfortable with your chosen stack?"];
+var item0=["CONTENT CHALLENGES", "How will you mitigate this challenge?", "What content challenges do you anticipate?", "By 'content' we mean the text and images of the site. Will this present a challenge for you? How will you deal with the challenge of finding the text and images you need?"];
 var item1=["TECH CHALLENGES", "How will you mitigate this challenge?", "Are you comfortable with your chosen stack?"];
-var item2=["API CHALLENGES", "How will you mitigate this challenge?", "Provide a link to the API documentation.", "You need to determine the limitations of the API before you start work. You don't want to discover them in Week 5."];
-var item3=["TIMESHEET", "Categorizing your total hours is helpful in addition to total project hours.", "Missing Week 1."];
-var item4=["GITHUB LINK", "Good work on your README!", "Please set up a repo for your project and add a README as soon as possible. We will use this to assess progress each week."];
+var item2=["API CHALLENGES", "How will you mitigate this challenge?", "Provide a link to the API documentation.", "You need to determine the limitations of the API before you start work. How much will it cost? How many hits per ?"];
+var item3=["OVERALL", "Very well thought out. Good work!", "Formalize this information in a document with headings - rather than a long list of items. Use the provided template to help with this."];
 
 
-feedBack.push(item0, item1, item2, item3, item4);
+feedBack.push(item0, item1, item2, item3);
