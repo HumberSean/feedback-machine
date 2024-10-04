262d325f4268c7432c5570f8a97150135954e54e
Sean Doyle <sean.doyle@humber.ca>
2024-10-04 13:57:12 -0400

    Add Capstone Usability Report Assessment

---
+++ CP/CP-UsabilityReport.js
@@ -0,0 +1,12 @@
+//CAPSTONE - Usability Report
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Very nice work!"];
+var item1=["REPORT DOCUMENT", "Document is missing sections. Use the template provided in the Usability course.", "Very well organized."];
+var item2=["SCENARIOS/TASKS", "Missing admin side task.", "Missing public side task.", "Provide specific values for your user to enter."];
+var item3=["ASSESSMENT/SUMMARY", "Vary astute assessment.", "What specific actions will you take to address the identified issues?", "Prioritize your results. What is urgent or most important?"];
+
+
+feedBack.push(item0, item1, item2, item3);
