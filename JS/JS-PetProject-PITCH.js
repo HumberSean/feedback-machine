08271f8553fda548f924ad1c57890388cf285647
Sean W Doyle <sean.doyle@humber.ca>
2024-10-17 07:22:38 -0400

    Add Pet Project Pitch and blank JS template file.

---
+++ JS/JS-PetProject-PITCH.js
@@ -0,0 +1,12 @@
+//JS Pet Project Pitch Blackboard Submission
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+var item1=["MUST HAVE", "We specified", "Let's say", "+="];
+var item2=["SHOULD HAVE", "+="];
+var item3=["NICE TO HAVE"];
+
+
+feedBack.push(item0, item1, item2, item3);
---
+++ JS/JS-TEMPLATE.js
@@ -0,0 +1,8 @@
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+var item1=["PART 1"];
+var item2=["PART 2"];
+
+
+feedBack.push(item0, item1, item2);
