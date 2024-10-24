23d1d4bdb2ec5be8f639490b45bee17ee44e9a1b
Sean W Doyle <sean.doyle@humber.ca>
2024-10-24 17:32:22 -0400

    Update JS Pet Project Pitch

--- JS/JS-PetProject-PITCH.js
+++ JS/JS-PetProject-PITCH.js
@@ -1,12 +1,14 @@
 //JS Pet Project Pitch Blackboard Submission
+//
 
 
 var feedBack = [];
 
-var item0=["FEEDBACK"];
+var item0=["FEEDBACK", "Project was not approved by the deadline.", "Let's say are hard coded and the user can add more. If the page is refreshed, the new content disappears and that's okay."];
 var item1=["MUST HAVE", "We specified", "Let's say", "+="];
 var item2=["SHOULD HAVE", "+="];
 var item3=["NICE TO HAVE"];
+var item4=["NEXT STEPS", "Please proceed with your Figma mockup."];
 
 
-feedBack.push(item0, item1, item2, item3);
+feedBack.push(item0, item1, item2, item3, item4);
