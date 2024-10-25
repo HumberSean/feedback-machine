8dee343adaf440f3370452b239545439aff8d607
Sean W Doyle <sean.doyle@humber.ca>
2024-10-25 10:45:31 -0400

    Update JS Pet Project Pitch.

--- JS/JS-PetProject-PITCH.js
+++ JS/JS-PetProject-PITCH.js
@@ -4,10 +4,10 @@
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Project was not approved by the deadline.", "Let's say are hard coded and the user can add more. If the page is refreshed, the new content disappears and that's okay."];
-var item1=["MUST HAVE", "We specified", "Let's say", "+="];
-var item2=["SHOULD HAVE", "+="];
-var item3=["NICE TO HAVE"];
+var item0=["FEEDBACK", "Project was not approved by the deadline.", "Let's say are hard coded and the user can add more. If the page is refreshed, the new content disappears and that's okay.", "Missing required details: What is the name of your app? What does your app do for users (who is it for and what does it do)?", "What are its features?", "What are its most important features (Must Have)?", "What would a user expect to be there (Should Have)?", "What are some &quot;bells and whistles&quot; that you might add in a future version (Nice to Have)?", "Your app name is misleading. It does not describe what your app actually does."];
+var item1=["MUST HAVE", "We specified", "Let's say", "+=", "Move to here."];
+var item2=["SHOULD HAVE", "+=", "Move to here."];
+var item3=["NICE TO HAVE", "Move to here."];
 var item4=["NEXT STEPS", "Please proceed with your Figma mockup."];
 
 
