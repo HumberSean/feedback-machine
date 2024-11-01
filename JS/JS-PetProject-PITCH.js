d4d8b5fbea503f60a4bd88ca67e2139a6466a962
Sean W Doyle <sean.doyle@humber.ca>
2024-11-01 10:03:26 -0400

    Update Pitch feedback

--- JS/JS-PetProject-PITCH.js
+++ JS/JS-PetProject-PITCH.js
@@ -4,7 +4,7 @@
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Project was not approved by the deadline.", "Let's say are hard coded and the user can add more. If the page is refreshed, the new content disappears and that's okay.", "Missing required details: What is the name of your app? What does your app do for users (who is it for and what does it do)?", "What are its features?", "What are its most important features (Must Have)?", "What would a user expect to be there (Should Have)?", "What are some &quot;bells and whistles&quot; that you might add in a future version (Nice to Have)?", "Your app name is misleading. It does not describe what your app actually does."];
+var item0=["FEEDBACK", "Project was not approved by the deadline.", "Let's say are hard coded and the user can add more. If the page is refreshed, the new content disappears and that's okay.", "Missing required details: What is the name of your app? What does your app do for users (who is it for and what does it do)?", "What are its features?", "What are its most important features (Must Have)?", "What would a user expect to be there (Should Have)?", "What are some &quot;bells and whistles&quot; that you might add in a future version (Nice to Have)?", "Your app name is misleading. It does not describe what your app actually does.", "&quot;user-friendly&quot; is not a feature. What feature makes it user-friendly?", "&quot;seamless&quot; is not a feature. What feature makes it seamless?"];
 var item1=["MUST HAVE", "We specified", "Let's say", "+=", "Move to here."];
 var item2=["SHOULD HAVE", "+=", "Move to here."];
 var item3=["NICE TO HAVE", "Move to here."];
