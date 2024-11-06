51839e0649eb61621fc9038e8c430f826232922c
Sean W Doyle <sean.doyle@humber.ca>
2024-11-06 17:40:43 -0500

    Add JS Lab - Forms

---
+++ JS/JS-LAB-Forms.js
@@ -0,0 +1,13 @@
+//JS LAB 7 - FORMS
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+var item1=["PART 1: Form Submission"];
+var item2=["PART 2: Form Validation", "Missing focus() on invalid element.", "Validate the fields in the order that the user sees them, i.e. the dropdown should validate first.", "Use the provided span elements to create your message.", "Don't set the output values until you have validated.", "Message output errors."];
+var item3=["EXTRA CHALLENGE: SHIPPING", "Bravo!", "The shipping speed dropdown needs validation.", "You can grab the text from the selected dropdown (see the back of your notes page).", "You can grab the shipping cost from the selected dropdown (see the back of your notes page)."];
+var item4=["EXTRA CHALLENGE: OBJECT", "Bravo!", "Don't assign input values to the object until you have validated the input."];
+
+
+feedBack.push(item0, item1, item2, item3, item4);
