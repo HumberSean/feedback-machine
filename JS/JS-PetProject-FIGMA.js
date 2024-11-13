f422e565882a1c44e2ce522f766dfe1e6fcf00a8
Sean Doyle <sean.doyle@humber.ca>
2024-11-13 16:59:36 -0500

    Add Pet Project Figma assessment.

---
+++ JS/JS-PetProject-FIGMA.js
@@ -0,0 +1,13 @@
+//PET PROJECT FIGMA
+//
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+var item1=["FIGMA", "Label your pages.", "Nicely labeled pages.", "Great job on the interactivity of your mock up!"];
+var item2=["USER EXPERIENCE", "Missing important information.", "Missing title, what is the name of your app?", "How do you use this app? Provide instructions."];
+var item3=["RESUBMIT", "Link broken", "Access request sent. Please resubmit as soon as you have granted me access."];
+
+
+feedBack.push(item0, item1, item2, item3);
