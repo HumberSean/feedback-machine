ef6a9804936c658d761ff062c2c8adcf7520afd3
Sean Doyle <sean.doyle@humber.ca>
2024-11-15 06:41:49 -0500

    Add JS Pet Project Final file

---
+++ JS/JS-PetProject-FINAL.js
@@ -0,0 +1,13 @@
+//JavaScript Pet Projects
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job.", "I would like to show this to future students. Please email me and let me know that I have your permission.", "This will be a great item for your portfolio!"];
+var item1=["FEATURES", "Nice use of your functions!", "A significant technical challenge - well done!", "Missing Should Have:", "Missing Must Have:", "alert() is not allowed. Use your DOM to message your users."];
+var item2=["DESIGN", "Needs some helper text so the user knows what to do.", "Some UX issues here. Run this with a person and you will discover most of them.", "Very confusing elements.", "Very well thought out with plenty of helper text.", "Fontawesome is not allowed for this project.", "Bootstrap is not allowed for this project."];
+var item3=["ERRORS", "You have an error on load. Keep your console open while you work to catch these errors."];
+var item4=["PROFESSIONAL PRACTICE", "Add helpful comments to mark the sections of your code and significant logic pieces. I encourage you to do this first (&quot;pseudocode&quot;) before you start your work.", "Use comments in your HTML to mark the sections to make editing easier.", "Use comments in your CSS to mark sections and components to make navigation of the file easier.", "Use comments in your js to mark sections of your code and to explain your functions and code blocks."];
+var item5=["SEPARATION OF CONCERNS", "Keep click listeners in js, not inline in HTML.", "Keep JavaScript in your .js files, not the HTML file.", "Keep styling in your CSS file."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5);
