233b7793de2d7315df99fb801996f80384b0f2c3
Sean Doyle <sean.doyle@humber.ca>
2024-11-14 21:22:08 -0500

    Update JS Figma

--- JS/JS-PetProject-FIGMA.js
+++ JS/JS-PetProject-FIGMA.js
@@ -5,9 +5,11 @@
 var feedBack = [];
 
 var item0=["FEEDBACK"];
-var item1=["FIGMA", "Label your pages.", "Nicely labeled pages.", "Great job on the interactivity of your mock up!"];
-var item2=["USER EXPERIENCE", "Missing important information.", "Missing title, what is the name of your app?", "How do you use this app? Provide instructions."];
-var item3=["RESUBMIT", "Link broken", "Access request sent. Please resubmit as soon as you have granted me access."];
+var item1=["FIGMA", "Nicely labeled frames.", "Your idea has been clearly communicated.", "Great job on the interactivity of your mock up!", "Label each frame with a short descriptor so that we have context for what we are looking at.", "Frame missing:"];
+
+var item2=["USER EXPERIENCE",  "Missing title, what is the name of your app?", "How do you use this app? Provide instructions.", "How do you use this app? All it needs is a sentence.", "Missing important information.", "No login or registration required for the MVP. Get rid of it."];
+
+var item3=["RESUBMIT", "Access request sent. Please resubmit as soon as you have granted me access.", "Link broken"];
 
 
 feedBack.push(item0, item1, item2, item3);
