0920a4896394f7426f41887201aa802eec8f2953
Sean W Doyle <sean.doyle@humber.ca>
2024-11-15 06:36:36 -0500

    Add JS Lab Date and Time

---
+++ JS/JS-LAB-8-DATEandTIMER.js
@@ -0,0 +1,11 @@
+//JS Lab 8 Math, Date, and Timers
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Good job"];
+var item1=["PART 1", "Don't hard code today's date, grab the dynamic date with var x = new Date();", "Deadline is 6pm (18:00:00)"];
+var item2=["PART 2", "And the extra &quot;0&quot; too! Well done!", "Where did my colons go between the time values?", "We should not see the time until start is clicked (#2.3)", "I should be able to restart the clock after stopping (#2.5)."];
+
+
+feedBack.push(item0, item1, item2);
