0972c9eef4b7451e61351985b9ea1d7f6643ba57
Sean W Doyle <sean.doyle@humber.ca>
2024-11-24 14:13:34 -0500

    Update JS Date/Time lab

--- JS/JS-LAB-8-DATEandTIMER.js
+++ JS/JS-LAB-8-DATEandTIMER.js
@@ -1,11 +1,12 @@
 //JS Lab 8 Math, Date, and Timers
+//
 
 
 var feedBack = [];
 
 var item0=["FEEDBACK", "Good job"];
-var item1=["PART 1", "Don't hard code today's date, grab the dynamic date with var x = new Date();", "Deadline is 6pm (18:00:00)"];
-var item2=["PART 2", "And the extra &quot;0&quot; too! Well done!", "Where did my colons go between the time values?", "We should not see the time until start is clicked (#2.3)", "I should be able to restart the clock after stopping (#2.5)."];
+var item1=["PART 1", "Don't hard code today's date, grab the dynamic date with var x = new Date();", "Use .toDateString() to get the date part of the timesstamp.", "Deadline is 6pm (18:00:00)"];
+var item2=["PART 2", "We should not see the time until start is clicked (#2.3)","This should show the current time. Use new Date (); to grab a timestamp every second, then pick out the hours, minutes and seconds.", "Where did my colons go between the time values?",  "I should be able to restart the clock after stopping (#2.5).", "And the extra &quot;0&quot; too! Well done!"];
 
 
 feedBack.push(item0, item1, item2);
