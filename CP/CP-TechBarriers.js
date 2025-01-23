f3a2d5f4a376592ab913338c299cd4c977a66045
Sean W Doyle <sean.doyle@humber.ca>
2025-01-23 17:07:06 -0500

    Add tech barriers feedback

--- CP/CP-TechBarriers.js
+++ CP/CP-TechBarriers.js
@@ -1,12 +1,13 @@
 //CAPSTONE-TECHNICAL BARRIERS ASSESSMENT
+//
 
 
 var feedBack = [];
 
-var item0=["CONTENT CHALLENGES", "How will you mitigate this challenge?", "What content challenges do you anticipate?", "By 'content' we mean the text and images of the site. Will this present a challenge for you? How will you deal with the challenge of finding the text and images you need?"];
-var item1=["TECH CHALLENGES", "How will you mitigate this challenge?", "Are you comfortable with your chosen stack?"];
+var item0=["CONTENT CHALLENGES", "What content challenges do you anticipate?", "How will you mitigate this challenge?", "By 'content' we mean the text and images of the site. Will this present a challenge for you? How will you deal with the challenge of finding the text and images you need?"];
+var item1=["TECH CHALLENGES", "What challenges do you anticipate with your selected technologies?", "How will you mitigate these challenges?", "Are you comfortable with your chosen stack?", ];
 var item2=["API CHALLENGES", "How will you mitigate this challenge?", "Provide a link to the API documentation.", "You need to determine the limitations of the API before you start work. How much will it cost? How many hits per ?"];
-var item3=["OVERALL", "Very well thought out. Good work!", "Formalize this information in a document with headings - rather than a long list of items. Use the provided template to help with this."];
+var item3=["OVERALL", "Very well thought out. Good work!", "Formalize this information in a document with headings - rather than a long list of items. Use the provided template to help with this.", "You have ignored the requirement to remove highlighted text. Please pay careful attention to requirements. In this document, we need to know you have not overlooked anything."];
 
 
 feedBack.push(item0, item1, item2, item3);
