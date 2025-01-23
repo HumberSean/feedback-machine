d8d91f7ce62c1b65a06626922f1000ccbad20c0b
Sean W Doyle <sean.doyle@humber.ca>
2025-01-23 14:58:07 -0500

    Add User testing date

--- CP/CP-RequirementsDoc.js
+++ CP/CP-RequirementsDoc.js
@@ -1,7 +1,6 @@
 //CAPSTONE: Requirements Document
 //
 
-
 var feedBack = [];
 
 var item0=["PROJECT PURPOSE", "Good job on this - who, what for, and summary of features.", "Needs detail. Who is this for and what can they do?", "Very well thought out."];
@@ -11,7 +10,7 @@ var item3=["FEATURE LIST", "Needs featured ordered by Must Have, Should Have, an
 var item4=["DB DESCRIPTION AND DIAGRAMS", "If a user can be deleted, sometimes a 'blocked' field in the DB is better than actually deleting the record.", "Missing dataflow diagrams.", "Separate the data flows for the different functionality.", "Missing database schema"];
 var item5=["WIREFRAMES", "Missing wireframes for described features.", "Nice detail in the wireframes.", "Wireframes need more detail and labels.", "Where does user go at end of data flow? What page or view?", "Wireframes show features not listed in feature list.", "Figma link does not work."];
 var item6=["RESUBMISSION", "You can resubmit before next class if you like.", "This document needs to be updated to reflect this feedback, and will be re-graded.", "Please highlight your changes in yellow (or use Word Track Changes) so that I can find your updates easily. Thank you!"];
-var item7=["TIMELINE", "Needs more detail.", "Difficulty: evaluate the difficulty of each task", "Due Date: set a target date for each task", "Missing time to plan, execute and report on user testing.", "Consider leaving authentication until after you have something working.", "Update to reflect changes made to requirements document.", "Don't leave deployment to Week 7.", "This document is not your time sheet (which tracks your time). The timeline is your schedule of what you plan to accomplish each week. Please complete and resubmit.", "Missing deployment. Do not leave this to the last week as there are often surprises that occur when moving from local to the internet.", "Missing user testing of Figma prototype."];
+var item7=["TIMELINE", "Needs more detail.", "Difficulty: evaluate the difficulty of each task", "Due Date: set a target date for each task", "Missing time to plan, execute and report on user testing.", "User testing will be in Week 4.", "Consider leaving authentication until after you have something working.", "Update to reflect changes made to requirements document.", "Don't leave deployment to Week 7.", "This document is not your time sheet (which tracks your time). The timeline is your schedule of what you plan to accomplish each week. Please complete and resubmit.", "Missing deployment. Do not leave this to the last week as there are often surprises that occur when moving from local to the internet.", "Missing user testing of Figma prototype."];
 
 
 feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
