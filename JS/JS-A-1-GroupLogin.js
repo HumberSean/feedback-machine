ae6eead6d5551df96ec2fc98dcba56f54bc67b9a
Sean W Doyle <sean.doyle@humber.ca>
2025-01-28 08:10:19 -0500

    Update JS A 1

--- JS/JS-A-1-GroupLogin.js
+++ JS/JS-A-1-GroupLogin.js
@@ -1,13 +1,15 @@
 //JavaScript Assignment 1 - Group Login
 //
 
+//
+
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Put all JavaScript in the required external file (#1).", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit."];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Put all JavaScript in the required external file (#1).", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "We have not covered objects or arrays yet, so I would not expect you to use them. Please pay careful attention to requirements to ensure you are delivering the requested skills."];
 var item1=["VALIDATE TEAM NUMBER", "There should only be one correct team number (#4). Your code does not check for one correct number.", "Code does not handle invalid input (#3, #7).", "I should not see a second invalid message popup after the first one for a wrong team number."];
-var item2=["ASK FOR NAME", "Code does not handle invalid input (#3, #7).", "I would expect to enter my name with an uppercase letter, but your code says it's invalid."];
-var item3=["GREET TEAM MEMBER", "Members should be greeted by their full name (#6).", "A &quot;set list&quot; of known values should use a switch.", "I love the use of variables in your switch for your team members!"];
+var item2=["ASK FOR NAME", "Code does not handle invalid input (#3, #7).", "I would expect to enter my name with an uppercase letter, but your code says it's invalid.", "A switch would be the more appropriate structure given a &quot;set list&quot; of known values to check against."];
+var item3=["GREET TEAM MEMBER", "Members should be greeted by their full name (#6).", "I love the use of variables in your switch for your team members!"];
 var item4=["INVALID DATA HANDLING", "Does not handle null (cancel) when asked for first name (#7). Keep your console open while you work to catch these."];
 var item5=["PROFESSIONAL PRACTICE", "Add helpful comments to mark the sections of your code and significant logic pieces. I encourage you to do this first (&quot;pseudocode&quot;) before you start your work.", "Missing responses to professional practice questions. Please respond to them in future assignments.", "Please email me if you are having trouble with this. We can make time to get you caught up. Each week will build on the previous week, so do not let yourself fall behind."];
 
