6ee652aec75dbf3520f49a4f374ca10a02f5ade2
Sean Doyle <sean.doyle@humber.ca>
2025-03-02 12:33:58 -0500

    Add JS Lab General feedback file.

---
+++ JS/JS-LAB-GENERAL.js
@@ -0,0 +1,15 @@
+//General JavaScript Lab Feedback
+
+
+var feedBack = [];
+
+var item0=["FILE SUBMISSION"];
+var item1=["FEEDBACK"];
+var item2=["PART 1", "Nice work!"];
+var item3=["PART 2", "Nice work!"];
+var item4=["PART 3"];
+var item5=["STRETCH GOAL"];
+var item6=["LATE PENALTY", "As an in-class lab, grades are not given outside of class without discussion in advance. Feedback is always given in the interest of advancement."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6);
