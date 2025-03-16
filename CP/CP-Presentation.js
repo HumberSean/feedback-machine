97527d992ee4f18e049d79661ac4ae4b62289214
Sean W Doyle <sean.doyle@humber.ca>
2025-03-16 11:13:56 -0400

    Add Capstone Presentations Feedback file.

---
+++ CP/CP-Presentation.js
@@ -0,0 +1,16 @@
+//CAPSTONE: Project Presentation
+//
+
+
+var feedBack = [];
+
+var item0=["PROJECT DEPLOYMENT"];
+var item1=["PROJECT PURPOSE", "Who is this site for?", "A technical description, but missing the human problem it's solving."];
+var item2=["TECHNOLOGY USED", "What is your tech stack?"];
+var item3=["FEATURE LEVEL ACHIEVED", "How much of your original plan did you achieve? Must Have; Should Have; Nice to Have?"];
+var item4=["DEMO OF MAIN FEATURES", "Missing"];
+var item5=["UX TESTING INSIGHTS", "What did you learn, what changes did you make based on your user testing?"];
+var item6=["NEXT STEPS", "What are the future plans for this app?"];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6);
