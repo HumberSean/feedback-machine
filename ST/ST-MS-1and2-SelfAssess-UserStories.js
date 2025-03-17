bac29c7706003e54f790ebdcea89c1fc90f02636
Sean W Doyle <sean.doyle@humber.ca>
2025-03-17 16:22:03 -0400

    Add Spec Topix Milestone 1 to Milestone 2 file and rename.

---
+++ ST/ST-MS-1and2-SelfAssess-UserStories.js
@@ -0,0 +1,16 @@
+//SPECIAL TOPICS MILESTONES 1 AND 2
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Very well thought out.", "What is your role? It is not clear from your responses.", "I would like you to not use GenAI for coding. Find examples or tutorials and cite them in your code.", "This is a list of your user stories that the others will complete for you. We need you to create a list of user stories that you will complete for the other roles. For example, what are the user stories for the Player that your role will be responsible for?", "Focus on the user stories that you will be responsible for. This will be your To Do list.", "Mobile first. Ignore desktop; no hover states.", "We need you to create User Stories from the perspective of the other roles. Please review the Milestone requirements for examples.", "Missing:"];
+var item1=["THE PLAYER", "Missing physical game components."];
+var item2=["DEVOPS"];
+var item3=["DESIGNER"];
+var item4=["LEAD DEV"];
+var item5=["DEVELOPER"];
+var item6=["RADIO TEAM"];
+var item7=["NEXT STEPS", "Please review the roles descriptions and email me to clarify any confusions.", "Please incorporate the feedback and resubmit.", "No need to include Instructor - but thank you!!!", "Please incorporate the feedback to add specifics to your responses and resubmit."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
