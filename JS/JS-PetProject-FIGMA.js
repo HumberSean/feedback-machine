9f2169fc794f4f3c57a79316887252e084bd43b5
Sean W Doyle <sean.doyle@humber.ca>
2025-03-25 08:34:26 -0400

    Update JS Figma feedback; update Spec Topix M1 and 2 feedback.

--- JS/JS-PetProject-FIGMA.js
+++ JS/JS-PetProject-FIGMA.js
@@ -4,10 +4,10 @@
 
 var feedBack = [];
 
-var item0=["FEEDBACK"];
-var item1=["FIGMA", "Nicely labeled frames.", "Your idea has been clearly communicated.", "Great job on the interactivity of your mock up!", "Label each frame with a short descriptor so that we have context for what we are looking at.", "Frame missing:"];
-var item2=["USER EXPERIENCE", "Missing title, what is the name of your app?", "How do you use this app? Provide instructions.", "How do you use this app? All it needs is a sentence.", "Missing important information.", "No login or registration required for the MVP. Get rid of it.", "Very nice design.", "Great use of images", "Great use of colour."];
-var item3=["RESUBMIT", "Access request sent. Please resubmit as soon as you have granted me access.", "Link broken"];
+var item0=["FEEDBACK", "This will be a great piece for your portfolio!"];
+var item1=["FIGMA", "Nicely labeled frames.", "Your idea has been clearly communicated.", "Great job on the interactivity of your mock up!", "Label each frame with a short descriptor so that we have context for what we are looking at.", "Frame missing:", "Feature missing:"];
+var item2=["USER EXPERIENCE", "Missing title, what is the name of your app?", "How do you use this app? Provide instructions.", "How do you use this app? All it needs is a sentence.", "Missing important information.", "No login or registration required for the MVP. Get rid of it.", "Very nice design.", "Great use of images.", "Great use of colour."];
+var item3=["RESUBMIT", "Access request sent. Please resubmit as soon as you have granted me access.", "Link broken", "Please resubmit once incorporating feedback."];
 
 
 feedBack.push(item0, item1, item2, item3);
--- ST/ST-MS-1and2-SelfAssess-UserStories.js
+++ ST/ST-MS-1and2-SelfAssess-UserStories.js
@@ -1,9 +1,10 @@
 //SPECIAL TOPICS MILESTONES 1 AND 2
+//
 
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Very well thought out.", "What is your role? It is not clear from your responses.", "I would like you to not use GenAI for coding. Find examples or tutorials and cite them in your code.", "This is a list of your user stories that the others will complete for you. We need you to create a list of user stories that you will complete for the other roles. For example, what are the user stories for the Player that your role will be responsible for?", "Focus on the user stories that you will be responsible for. This will be your To Do list.", "Mobile first. Ignore desktop; no hover states.", "We need you to create User Stories from the perspective of the other roles. Please review the Milestone requirements for examples.", "Missing:"];
+var item0=["FEEDBACK", "Very well thought out.", "What is your role? It is not clear from your responses.", "I would like you to not use GenAI for coding. Find examples or tutorials and cite them in your code.", "This is a list of your user stories that the others will complete for you. We need you to create a list of user stories that you will complete for the other roles. For example, what are the user stories for the Player that your role will be responsible for?", "Focus on the user stories that you will be responsible for. This will be your To Do list.", "Mobile first. Ignore desktop; no hover states.", "We need you to create User Stories from the perspective of the other roles. Please review the Milestone requirements for examples.", "Missing:", "You are missing some important stories for your role. Please see my new spreadsheet in Milestone 2 and add the missing stories."];
 var item1=["THE PLAYER", "Missing physical game components."];
 var item2=["DEVOPS"];
 var item3=["DESIGNER"];
