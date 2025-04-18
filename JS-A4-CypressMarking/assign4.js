c29ea43668474332f5b1133bb711a0a1cfc0a7d1
Sean Doyle <sean.doyle@humber.ca>
2025-04-18 15:17:30 -0400

    Update feedback

--- JS-A4-CypressMarking/assign4.js
+++ JS-A4-CypressMarking/assign4.js
@@ -1,90 +1,96 @@
-//Assignment 4 - Registration Form
-window.onload = function(){
-	//GET REQUIRED ELEMENTS
-	var formHandle = document.forms.ixdForm;
-	var headSection = document.getElementById("welcome");
-	var resultSection = document.getElementById("result");
-	var out_Fname = document.getElementById("result__Fname");
-	var out_Lname = document.getElementById("result__Lname");
-	var out_id = document.getElementById("result__id");
-	var out_program = document.getElementById("result__program");
-	// out_deliver = document.getElementById("result__delivery");
-	var err_deliver = document.getElementById("caption_deliver");
-	var out_project = document.getElementById("result__project");
-	var err_project = document.getElementById("caption_project");
+window.onload = function() {
 
+  var form = document.forms["ixdForm"];
 
-//FUNCTION TO VALIDATE FORM ONCE SUBMITTED
-	function validateForm() {
-		//Get form elements
-		var f_Fname = formHandle.f__fName;
-		var f_Lname = formHandle.f__lName;
-		var f_id = formHandle.f__id;
-		var f_program = formHandle.f__program;
-		//var f_deliver = formHandle.f__deliver;
-		var f_project = formHandle.f__project;
-		
-	//==== VALIDATION ====
-		//First Name
-		if (f_Fname.value === "") {
-			f_Fname.style.background = "red";
-			f_Fname.focus();
-			return false;
-		} else {f_Fname.style.background = "white";}
-		
-		//Last Name
-		if (f_Lname.value === "") {
-			f_Lname.style.background = "red";
-			f_Lname.focus();
-			return false;
-		} else {f_Lname.style.background = "white";}
+  form.onsubmit = function(event) {
+    event.preventDefault();
 
-		//ID
-		var HIDregex = /^N\d{8}$/i;
-		if (!HIDregex.test(f_id.value)) {
-			f_id.style.background = "red";
-			f_id.focus();
-			return false;
-		} else {f_id.style.background = "white";}
+    var fname = document.getElementById("in_fName").value;
+    var lname = document.getElementById("in_lName").value;
+    var id = document.getElementById("in_id").value;
+    var program = document.getElementById("in_program").value;
+    var projectOptions = document.getElementsByName("f__project");
 
-		//Program
-		var HIDregex = /N\d{8}$/i;
-		if (f_program.value === "X") {
-			f_program.style.background = "red";
-			f_program.focus();
-			return false;
-		} else {f_program.style.background = "white";}
+    document.getElementById("in_fName").style.backgroundColor = "";
+    document.getElementById("in_lName").style.backgroundColor = "";
+    document.getElementById("in_id").style.backgroundColor = "";
+    document.getElementById("in_program").style.backgroundColor = "";
+    document.getElementById("caption_project").style.backgroundColor = "";
 
+    var valid = true;
 
-		//Delivery
-		/* if (f_deliver.value === "") {
-			err_deliver.style.background = "red";
-			return false;
-		} else {err_deliver.style.background = "white";
-		} */
-		
-		//Project
-		if (f_project.value === "") {
-			err_project.style.background = "red";
-			return false;
-		} else {err_project.style.background = "white";}
-		
-		
-		//SHOW/HIDE PANELS AND OUTPUT RESULTS
-		headSection.style.display = "none";
-		formHandle.style.display = "none";
-		resultSection.style.display = "block";
-		
-		out_Fname.innerHTML = f_Fname.value;
-		out_Lname.innerHTML = f_Lname.value;
-		out_id.innerHTML = f_id.value;
-		out_program.innerHTML = f_program.options[f_program.selectedIndex].text;
-		//out_deliver.innerHTML = f_deliver.value;
-		out_project.innerHTML = f_project.value;
+    // First Name
+    if (fname === "") {
+      document.getElementById("in_fName").style.backgroundColor = "red";
+      document.getElementById("in_fName").focus();
+      valid = false;
+    }
 
-		return false;
-	}//END validateForm
+    // Last Name
+    else if (lname === "") {
+      document.getElementById("in_lName").style.backgroundColor = "red";
+      document.getElementById("in_lName").focus();
+      valid = false;
+    }
 
+    // Humber ID (empty + regex)
+    else if (id === "") {
+      document.getElementById("in_id").style.backgroundColor = "red";
+      document.getElementById("in_id").focus();
+      valid = false;
+    } else {
+      var idPattern = /^([nN])\d{8}$/;
+      if (!idPattern.test(id)) {
+        document.getElementById("in_id").style.backgroundColor = "red";
+        document.getElementById("in_id").focus();
+        alert("Invalid Humber ID.\nIt should start with 'N' or 'n' followed by 8 digits.");
+        valid = false;
+      }
+    }
 
-	formHandle.onsubmit = validateForm;
-};//end onload
+    // Program
+    if (valid && program === "X") {
+      document.getElementById("in_program").style.backgroundColor = "red";
+      document.getElementById("in_program").focus();
+      valid = false;
+    }
+
+    // Project selection
+    var projectChosen = false;
+    var projectValue = "";
+    for (var i = 0; i < projectOptions.length; i++) {
+      if (projectOptions[i].checked) {
+        projectChosen = true;
+        projectValue = projectOptions[i].value;
+      }
+    }
+    if (valid && !projectChosen) {
+      document.getElementById("caption_project").style.backgroundColor = "red";
+      valid = false;
+    }
+
+    // Show result if all inputs are valid
+    if (valid) {
+      var fullProgram = "";
+      if (program === "CS") {
+        fullProgram = "Content Strategy";
+      } else if (program === "CT") {
+        fullProgram = "Creative Technologies";
+      } else if (program === "UX") {
+        fullProgram = "User Experience Design";
+      } else if (program === "WD") {
+        fullProgram = "Web Development";
+      }
+
+      document.getElementById("welcome").style.display = "none";
+      document.getElementById("form").style.display = "none";
+      document.getElementById("result").style.display = "block";
+
+      document.getElementById("result__Fname").innerText = fname;
+      document.getElementById("result__Lname").innerText = lname;
+      document.getElementById("result__id").innerText = id;
+      document.getElementById("result__program").innerText = fullProgram;
+      document.getElementById("result__project").innerText = projectValue;
+    }
+  };
+};
