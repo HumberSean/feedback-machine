8298db50406af3776e99be02d0b38c812d932128
Sean Doyle <sean.doyle@humber.ca>
2025-04-18 10:24:27 -0400

    Add Special Topics M3 and 4 from M1 and 2.

--- ST/ST-MS-1and2-SelfAssess-UserStories.js
+++ ST/ST-MS-1and2-SelfAssess-UserStories.js
@@ -1,5 +1,5 @@
-//SPECIAL TOPICS MILESTONES
-//
+//SPECIAL TOPICS MILESTONES  1 AND 2
+//SELF-ASSESS AND USER STORIES
 
 
 var feedBack = [];
@@ -12,8 +12,5 @@ var item4=["LEAD DEV"];
 var item5=["DEVELOPER"];
 var item6=["RADIO TEAM"];
 var item7=["NEXT STEPS", "Please review the roles descriptions and email me to clarify any confusions.", "Please incorporate the feedback and resubmit.", "No need to include Instructor - but thank you!!!", "Please incorporate the feedback to add specifics to your responses and resubmit."];
-var item8=["MILESTONE 3.1", "These should be the user stories from your Milestone 2.", "Please update the status of your items once you have corrected them.", "Please update the status of your items regularly. I will be checking them through the project.", "Please save this online as a Google sheet and submit the link to that file. I will be using it to evaluate your work."];
-var item9=["MILESTONE 4.1", "Approved.", "Please proceed to complete and submit the Milestone-4-Project_Overview.docx document found in Milestone 4.2.", "This document must reach SAT status by the end of the semester so do not wait until the end to submit or may not have time to incorporate feedback and resubmit.", "Thank you, looking forward to seeing this project come to life!"];
 
-
-feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
---
+++ ST/ST-MS-3and4.js
@@ -0,0 +1,15 @@
+//SPECIAL TOPICS MILESTONES
+//
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+
+var item1=["MILESTONE 3.1", "These should be the user stories from your Milestone 2.", "Please update the status of your items once you have corrected them.", "Please update the status of your items regularly. I will be checking them through the project.", "Please save this online as a Google sheet and submit the link to that file. I will be using it to evaluate your work."];
+var item2=["MILESTONE 3.2"];
+var item3=["MILESTONE 4.1", "Approved.", "Please proceed to complete and submit the Milestone-4-Project_Overview.docx document found in Milestone 4.2.", "This document must reach SAT status by the end of the semester so do not wait until the end to submit or may not have time to incorporate feedback and resubmit.", "Please resubmit with the name of your app and a sentence that describes it.", "Provide more details beyond, \nWho is it for -specifically - and what can they do in your app?"];
+var item4=["MILESTONE 4.2", "Thank you, looking forward to seeing this project come to life!"];
+
+
+feedBack.push(item0, item1, item2, item3, item4);
