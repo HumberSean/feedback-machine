f37dd1b038bce348d6b3c58ebd96eb654c100e57
Sean Doyle <sean.doyle@humber.ca>
2025-04-20 11:50:35 -0400

    Add feedback

--- ST/ST-MS-3and4.js
+++ ST/ST-MS-3and4.js
@@ -1,14 +1,15 @@
 //SPECIAL TOPICS MILESTONES
 //3 PROJECT BACKLOG AND 4 CAPSTONE PROPOSAL
+//
 
 
 var feedBack = [];
 
 var item0=["FEEDBACK"];
 var item1=["MILESTONE 3.1", "These should be the user stories from your Milestone 2.", "Please update the status of your items once you have corrected them.", "Please update the status of your items regularly. I will be checking them through the project.", "Please save this online as a Google sheet and submit the link to that file. I will be using it to evaluate your work."];
-var item2=["MILESTONE 3.2"];
+var item2=["MILESTONE 3.2", "Missing user story:", "As a Player, I want to identify which cards are Chapters and which are Fragments.", "As a Player, I want to play the web app game on my mobile device.", "As a Player, I want to access the home page of the game.", "As a Player, I want to learn how to play the game.", "As a Player, I want to hear the introductory audio.", "As a Player, I want to hear the full chapter once solved.", "As a Player, I want to enter my guess for the order of the chapters.", "As a Player, I want to hear the full story once all chapters are solved.", "As a Player, I want to hear the epilogue audio.", "As a Player, I want to learn about the creators of the game.", "As a Player, I want to navigate through the app and game from any page.", "the Player needs to get to the home page of the app from the physical game. Usually it's an extra card, but I've seen a separate page, or a container for the cards with this info.", "The Player needs to get a sense of the game's theme and there is nothing more important than the title. Make sure we can tell the title of the project from the physical components.", "Player user stories must be complete by end of day Monday so that the Radio students can play the games.", "Please email me as soon as you have completed these user stories - and enter &quot;user stories complete&quot; in Milestone 3.2 on Blackboard."];
 var item3=["MILESTONE 4.1", "Approved.", "Please proceed to complete and submit the Milestone-4-Project_Overview.docx document found in Milestone 4.2.", "This document must reach SAT status by the end of the semester so do not wait until the end to submit or may not have time to incorporate feedback and resubmit.", "Please resubmit with the name of your app and a sentence that describes it.", "Provide more details beyond, Who is it for -specifically - and what can they do in your app?"];
-var item4=["MILESTONE 4.2", "Clarifications for the Requirements Document next term (but no need to resubmit now):\n", "MUST HAVE: ", "SHOULD HAVE: ", "NICE TO HAVE: ", "\nThank you, looking forward to seeing this project come to life!"];
+var item4=["MILESTONE 4.2", "Clarifications for the Requirements Document next term (but no need to resubmit now):", "MUST HAVE:", "SHOULD HAVE:", "NICE TO HAVE:", " Thank you, looking forward to seeing this project come to life!"];
 
 
 feedBack.push(item0, item1, item2, item3, item4);
