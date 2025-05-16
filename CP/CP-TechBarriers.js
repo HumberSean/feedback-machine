9c7305994e1feb961a34fbc094e0d6ce5891c3bc
Sean Doyle <sean.doyle@humber.ca>
2025-05-16 13:16:45 -0400

    Add feedback.

--- CP/CP-TechBarriers.js
+++ CP/CP-TechBarriers.js
@@ -4,9 +4,9 @@
 
 var feedBack = [];
 
-var item0=["CONTENT CHALLENGES", "What content challenges do you anticipate?", "How will you mitigate this challenge?", "By 'content' we mean the text and images of the site. Will this present a challenge for you? How will you deal with the challenge of finding the text and images you need?"];
-var item1=["TECH CHALLENGES", "What challenges do you anticipate with your selected technologies?", "How will you mitigate these challenges?", "Are you comfortable with your chosen stack?", ];
-var item2=["API CHALLENGES", "How will you mitigate this challenge?", "Provide a link to the API documentation.", "You need to determine the limitations of the API before you start work. How much will it cost? How many hits per ?"];
+var item0=["CONTENT CHALLENGES", "What content challenges do you anticipate?", "How will you mitigate this challenge?", "How will you mitigate these challenges?", "By 'content' we mean the text and images of the site. Will this present a challenge for you? How will you deal with the challenge of finding the text and images you need?"];
+var item1=["TECH CHALLENGES", "How comfortable are you with your chosen stack?", "What challenges do you anticipate with your selected technologies?", "How will you mitigate these challenges?"];
+var item2=["API CHALLENGES", "How will you mitigate this challenge?", "Provide a link to the API documentation.", "You need to determine the limitations of the API before you start work. How much will it cost? How many hits per ?", "What happens if those limits are exceeded? Your client will want to know."];
 var item3=["OVERALL", "Very well thought out. Good work!", "Formalize this information in a document with headings - rather than a long list of items. Use the provided template to help with this.", "You have ignored the requirement to remove highlighted text. Please pay careful attention to requirements. In this document, we need to know you have not overlooked anything."];
 
 
