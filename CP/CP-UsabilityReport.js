952740e185846bc1c80bddd4c0323dbd914e4c71
Sean W Doyle <sean.doyle@humber.ca>
2025-06-01 16:05:44 -0400

    Add more feedback items and a heading.

--- CP/CP-UsabilityReport.js
+++ CP/CP-UsabilityReport.js
@@ -1,12 +1,16 @@
 //CAPSTONE - Usability Report
 
+//
+
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Very nice work!"];
+var item0=["FEEDBACK", "Very nice work!", "You found target users - excellent!!"];
 var item1=["REPORT DOCUMENT", "Document is missing sections. Use the template provided in the Usability course.", "Very well organized."];
-var item2=["SCENARIOS/TASKS", "Missing admin side task.", "Missing public side task.", "Provide specific values for your user to enter.", "Provide details."];
-var item3=["ASSESSMENT/SUMMARY", "Very astute assessment.", "What specific actions will you take to address the identified issues?", "Prioritize your results. What is urgent or most important?"];
+var item2=["SCENARIOS/TASKS", "Missing admin side task.", "Missing public side task.", "Provide specific values for your user to enter.", "Provide details.", "Provide your scenarios."];
+var item3=["FINDINGS", "Missing details of interactions.", "Don't decide what they thought, what did you see/hear?"];
+var item4=["ASSESSMENT/SUMMARY", "Very astute assessment.", "What specific actions will you take to address the identified issues?", "Prioritize your results. What is urgent or most important?"];
 
 
-feedBack.push(item0, item1, item2, item3);
+
+feedBack.push(item0, item1, item2, item3, item4);
