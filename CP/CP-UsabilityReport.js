e21e8d5a4c5b82a98b08d46d22b9a24a60131df4
Sean W Doyle <sean.doyle@humber.ca>
2025-06-02 08:55:46 -0400

    Added more feedback.

--- CP/CP-UsabilityReport.js
+++ CP/CP-UsabilityReport.js
@@ -7,10 +7,9 @@ var feedBack = [];
 
 var item0=["FEEDBACK", "Very nice work!", "You found target users - excellent!!"];
 var item1=["REPORT DOCUMENT", "Document is missing sections. Use the template provided in the Usability course.", "Very well organized."];
-var item2=["SCENARIOS/TASKS", "Missing admin side task.", "Missing public side task.", "Provide specific values for your user to enter.", "Provide details.", "Provide your scenarios."];
-var item3=["FINDINGS", "Missing details of interactions.", "Don't decide what they thought, what did you see/hear?"];
-var item4=["ASSESSMENT/SUMMARY", "Very astute assessment.", "What specific actions will you take to address the identified issues?", "Prioritize your results. What is urgent or most important?"];
-
+var item2=["SCENARIOS/TASKS", "Missing admin-side task.", "Missing public-side task.", "Provide specific values for your user to enter.", "Provide details.", "Provide your scenarios."];
+var item3=["FINDINGS", "Missing details of interactions.", "You have provided the individual data, but we need your review of the overall aggregate data. E.g. &quot;2 out of 5 people had difficulty updating the product.&quot;,&quot;5 of 5 completed the task successfully.&quot;", "Don't decide what they thought, what did you see/hear?"];
+var item4=["ASSESSMENT/SUMMARY", "Very astute assessment.", "Missing your overall assessment and recommendations.", "What specific actions will you take to address the identified issues?", "Prioritize your results. What is urgent or most important?"];
 
 
 feedBack.push(item0, item1, item2, item3, item4);
