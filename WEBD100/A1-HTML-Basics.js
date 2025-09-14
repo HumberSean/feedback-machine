97e23dc3392206f6ef41b98dda23f52d892be727
Sean W Doyle <sean.doyle@humber.ca>
2025-09-14 13:48:36 -0400

    Add WEBD100 assignment 1

---
+++ WEBD100/A1-HTML-Basics.js
@@ -0,0 +1,9 @@
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+var item1=["HTML MARKUP"];
+var item2=["DOCUMENT & FILE FORMAT"];
+var item3=["EFFECTIVE CONTENT"];
+
+
+feedBack.push(item0, item1, item2, item3);
