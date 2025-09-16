f4b53d192d057a95af6ad387709d480e0c0e6aad
Sean W Doyle <sean.doyle@humber.ca>
2025-09-16 09:38:33 -0400

    Rename Lab 2 file, add feedback items.

---
+++ WD/WD-L2_tables-and-nav.js
@@ -0,0 +1,12 @@
+//WEBD 100 LAB 2: Tables and Navigation
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job."];
+var item1=["PART 1", "Don't forget to include all files when submitting. Image file missing.", "Step 2: Be sure to have quotes around the image file name...src=&quot;singapore-swimming-worlds.jpg&quot;", "Step 3: Missing em around 'source' at end of text on home page.", "Step 3: use strong and em elements to add emphasis around special text.", "Step 4: Be sure to close every tag in the table."];
+var item2=["PART 2", "All pages should have full navigation links for all pages.", "Page 3 missing from nav on home page."];
+var item3=["EXTRA CHALLENGES", "And the extra challenges too, well done!", "And the table caption too, well done!", "Use the caption element inside of the table to add a heading to a table.", "And the hyperlink too, well done!", "Hyperlink: If we are linking to an external site, use target='_blank' so that a new tab is opened.", "Put the a tags around the image to make the image the hyperlink."];
+
+
+feedBack.push(item0, item1, item2, item3);
