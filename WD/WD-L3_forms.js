609b35ce89818b4edc18c6b4638dfe1d56ae51e9
Sean Doyle <sean.doyle@humber.ca>
2025-09-18 15:06:09 -0400

    Add WEBD100 Lab 3 file

---
+++ WD/WD-L3_forms.js
@@ -0,0 +1,13 @@
+//WEBD 100 LAB 3: Forms
+//
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job.", "Rename the file from TEMPLATE to something meaningful for the page, e.g. feedback.html.", "Please pay careful attention to requirements.", "Please fix and resubmit."];
+var item1=["PART 1", "Don't forget to include all files when submitting. Image file missing.", "Don't forget name attributes for your form elements. These add tags to the input for the receiving server.", "fieldset goes inside the form element.", "Missing closing body tag.", "Don't forget a name attribute for your checkbox."];
+var item2=["PART 2"];
+var item3=["EXTRA CHALLENGES", "And the extra challenges too, well done!", "Nice extra touch with the required field!", "Fieldset! Well done."];
+
+
+feedBack.push(item0, item1, item2, item3);
