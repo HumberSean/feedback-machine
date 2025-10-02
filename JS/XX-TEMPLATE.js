069437ed875eb195376890330c6d6c9616b39f95
Sean Doyle <sean.doyle@humber.ca>
2025-10-02 13:06:19 -0400

    Create template file in root folder.

---
+++ JS/XX-TEMPLATE.js
@@ -0,0 +1,9 @@
+var feedBack = [];
+
+const item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit."];
+const item1=["PART 1"];
+const item2=["PART 2"];
+const item3=["PART 3"];
+
+
+feedBack.push(item0, item1, item2, item3);
