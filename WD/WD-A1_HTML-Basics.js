5e6eda66962dac55d455be27e0415b584d872393
Sean Doyle <sean.doyle@humber.ca>
2025-10-02 13:08:21 -0400

    Add more default feedback text.

--- WD/WD-A1_HTML-Basics.js
+++ WD/WD-A1_HTML-Basics.js
@@ -1,8 +1,8 @@
 var feedBack = [];
 
-var item0=["FEEDBACK"];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit."];
 var item1=["HTML MARKUP"];
-var item2=["DOCUMENT & FILE FORMAT"];
+var item2=["DOCUMENT &amp; FILE FORMAT"];
 var item3=["EFFECTIVE CONTENT"];
 
 
