e368afa7457438b2b8be554941ed651d116907c5
Sean Doyle <sean.doyle@humber.ca>
2025-10-03 09:09:48 -0400

    Tweak special characters.

--- WD/WD-A1_HTML-Basics.js
+++ WD/WD-A1_HTML-Basics.js
@@ -1,9 +1,16 @@
+//WEBD100 Assignment 1 HTML Basics
+
+
 var feedBack = [];
 
 var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit."];
-var item1=["HTML MARKUP"];
-var item2=["DOCUMENT &amp; FILE FORMAT"];
+var item1=["HTML MARKUP", "A &lt;q> element would be more appropriate for a single line quote.", "The &lt;a> tag should go inside the &lt;p> and around the &lt;img> because it is an inline element."];
+var item2=["DOCUMENT & FILE FORMAT", "Use tabs to visually show which elements are inside which elements."];
 var item3=["EFFECTIVE CONTENT"];
+var item4=["FORM", "Form element is missing action and method attributes."];
+var item5=["EXTERNAL LINKS", "Use the target=&quot;_blank&quot; attribute to open an external page in a new tab."];
+var item6=["LOGO LINK", "The logo should be the same from page to page."];
+var item7=["IMAGES", "Image file is too large for how it is being used."];
 
 
-feedBack.push(item0, item1, item2, item3);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
