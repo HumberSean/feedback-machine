045a6611b951d88dc8f07a9d43b8f98f3fdcc8b0
Sean Doyle <sean.doyle@humber.ca>
2025-10-03 09:02:37 -0400

    Add alt attribute bookmarklet

---
+++ alt_attribute-bookmarklet.js
@@ -0,0 +1,12 @@
+javascript:(()=>{
+	var x=document.getElementsByTagName("img");
+	var txt="<h1>alt Attribute Report</h1><table>";
+	Array.from(x).forEach((image)=>{
+		let at=image.getAttribute("alt")===null ? "<em>MISSING ALT</em>":image.getAttribute("alt");
+		txt+="<tr><td>";
+		txt+=image.src.substring(image.src.lastIndexOf('/')+1)+"</td><td>"+at+"</td></tr>";
+	});
+	txt+="</table>";
+	document.write("<html><head></head><body>"+txt+"</body></html>");
+})();
+//Use JSTool plugin, and JSMin to minify once ready to update bookmark.
