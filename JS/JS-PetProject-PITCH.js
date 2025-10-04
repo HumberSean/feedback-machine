c48d25ad6c8e08ab37ff5d9439400a506cda6557
Sean W Doyle <sean.doyle@humber.ca>
2025-10-04 16:27:21 -0400

    Added pitch by email feedback.

--- JS/JS-PetProject-PITCH.js
+++ JS/JS-PetProject-PITCH.js
@@ -9,6 +9,7 @@ var item1=["MUST HAVE", "We specified", "Let's say", "+=", "Move to here."];
 var item2=["SHOULD HAVE", "+=", "Move to here."];
 var item3=["NICE TO HAVE", "Move to here."];
 var item4=["NEXT STEPS", "Please proceed with your Figma mockup.", "Please revise based on feedback and resubmit."];
+var item5=["PROJECT APPROVED", "Please submit to Blackboard:", "The name of your app; A description of your app (what it does and who it's for);", "Your project features list (bullet points are fine) prioritized by &quot;Must Have&quot;, &quot;Should Have&quot; and &quot;Nice to Have&quot;.", "Thank you, looking forward to seeing your project!"];
 
 
-feedBack.push(item0, item1, item2, item3, item4);
+feedBack.push(item0, item1, item2, item3, item4, item5);
