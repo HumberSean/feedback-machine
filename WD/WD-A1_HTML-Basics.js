26d09c8fdc134da7b8e413086c6f2a761f9ca2c5
Sean W Doyle <sean.doyle@humber.ca>
2025-10-07 13:11:40 -0400

    Update feedback.

--- WD/WD-A1_HTML-Basics.js
+++ WD/WD-A1_HTML-Basics.js
@@ -1,16 +1,19 @@
 //WEBD100 Assignment 1 HTML Basics
 
+//
+
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "I appreciate "];
-var item1=["HTML MARKUP", "A &lt;q> element would be more appropriate for a single line quote.", "The &lt;a> tag should go inside the &lt;p> and around the &lt;img> because it is an inline element."];
-var item2=["DOCUMENT & FILE FORMAT", "Use tabs to visually show which elements are inside which elements."];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "I appreciate", "Review the rubric before submitting to see exactly what I'm looking for. Email me if you have any questions.", "For your next assignment, I want you to focus on technical accuracy."];
+var item1=["HTML MARKUP", "A <q> element would be more appropriate for a single line quote.", "The <a> tag should go inside the <p> and around the <img> because it is an inline element.", "Document elements are not properly nested. Head and body go inside the html element, head above body.", "Don't jump h elements, use the steps.", "All content that will show on the webpage (except the tab text) goes here between the opening and closing tags", "All content that will show on the webpage (except the tab text) goes between the opening and closing body tags.", "Make sure each tag is closed properly."];
+var item2=["DOCUMENT & FILE FORMAT", "Use tabs to visually show which elements are inside which elements.", "Home page file should be index.html", "Avoid spaces in your file names."];
 var item3=["EFFECTIVE CONTENT"];
-var item4=["FORM", "Form element is missing action and method attributes."];
+var item4=["FORM", "Form element is missing action and method attributes.", "name attributes should be unique for each input and not have spaces."];
 var item5=["EXTERNAL LINKS", "Use the target=&quot;_blank&quot; attribute to open an external page in a new tab."];
 var item6=["LOGO LINK", "The logo should be the same from page to page."];
-var item7=["IMAGES", "Image file is too large for how it is being used."];
+var item7=["IMAGES", "Image file is too large for how it is being used.", "Image files are too large for how they are being used", "Image files are too large for how they are being used.", "All images need an alt attribute."];
+var item8=["NAVIGATION", "Needs home option besides linked logo.", "Full navigation needs to be on every page of your website."];
 
 
-feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8);
