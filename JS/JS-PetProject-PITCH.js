246f303f959769a177d626903e1e0f2532a4074c
Sean W Doyle <sean.doyle@humber.ca>
2025-10-16 07:58:48 -0400

    Add pitch by email feedback.

--- JS/JS-PetProject-PITCH.js
+++ JS/JS-PetProject-PITCH.js
@@ -1,4 +1,4 @@
-//JS Pet Project Pitch Blackboard Submission
+//JS Pet Project (email) Pitch and Blackboard Submission
 //
 
 
@@ -9,7 +9,7 @@ var item1=["MUST HAVE", "We specified", "Let's say", "+=", "Move to here."];
 var item2=["SHOULD HAVE", "+=", "Move to here."];
 var item3=["NICE TO HAVE", "Move to here."];
 var item4=["NEXT STEPS", "Please proceed with your Figma mockup.", "Please revise based on feedback and resubmit."];
-var item5=["PROJECT APPROVED", "Please submit to Blackboard:", "The name of your app; A description of your app (what it does and who it's for);", "Your project features list (bullet points are fine) prioritized by &quot;Must Have&quot;, &quot;Should Have&quot; and &quot;Nice to Have&quot;.", "Thank you, looking forward to seeing your project!"];
+var item5=["PROJECT APPROVED", "Unfortunately, this is essentially a To Do list and, therefore, not permitted.", "What are your backup ideas? Just send bullet points.", "Let's say", "If the page is refreshed, the new items disappear (are not saved to a database).", "We see when the page first loads and the user can add more.", "Please submit to Blackboard:", "The name of your app; A description of your app (what it does and who it's for);", "Your project features list (bullet points are fine) prioritized by &quot;Must Have&quot;, &quot;Should Have&quot; and &quot;Nice to Have&quot;.", "Thank you, looking forward to seeing your project!"];
 
 
 feedBack.push(item0, item1, item2, item3, item4, item5);
