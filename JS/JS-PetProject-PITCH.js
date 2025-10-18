02c5edc6a951cab8e466c0bf03b0092cdb5f2819
Sean W Doyle <sean.doyle@humber.ca>
2025-10-18 17:35:30 -0400

    Update feedback.

--- JS/JS-PetProject-PITCH.js
+++ JS/JS-PetProject-PITCH.js
@@ -4,12 +4,12 @@
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Project was not approved by the deadline.", "Let's say are hard coded and the user can add more. If the page is refreshed, the new content disappears and that's okay.", "Missing required details: What is the name of your app? What does your app do for users (who is it for and what does it do)?", "What are its features?", "What are its most important features (Must Have)?", "What would a user expect to be there (Should Have)?", "What are some &quot;bells and whistles&quot; that you might add in a future version (Nice to Have)?", "Your app name is misleading. It does not describe what your app actually does.", "&quot;user-friendly&quot; is not a feature. What feature makes it user-friendly?", "&quot;seamless&quot; is not a feature. What feature makes it seamless?"];
-var item1=["MUST HAVE", "We specified", "Let's say", "+=", "Move to here."];
-var item2=["SHOULD HAVE", "+=", "Move to here."];
-var item3=["NICE TO HAVE", "Move to here."];
+var item0=["FEEDBACK", "Project was not approved by the deadline.", "Let's say are hard coded and the user can add more. If the page is refreshed, the new content disappears and that's okay.", "Missing required details: What is the name of your app? What does your app do for users (who is it for and what does it do)?", "What are its features?", "What are its most important features (Must Have)?", "What would a user expect to be there (Should Have)?", "What are some &quot;bells and whistles&quot; that you might add in a future version (Nice to Have)?", "Your app name is misleading. It does not describe what your app actually does.", "&quot;user-friendly&quot; is not a feature. What feature makes it user-friendly?", "&quot;seamless&quot; is not a feature. What feature makes it seamless?", "Great job!"];
+var item1=["MUST HAVE", "We specified ", "Let's say ", "+= ", "Move &quot;&quot; to here."];
+var item2=["SHOULD HAVE", "+= ", "Move &quot;&quot; to here."];
+var item3=["NICE TO HAVE", "+= ", "Move &quot;&quot; to here."];
 var item4=["NEXT STEPS", "Please proceed with your Figma mockup.", "Please revise based on feedback and resubmit."];
-var item5=["PROJECT APPROVED", "Unfortunately, this is essentially a To Do list and, therefore, not permitted.", "What are your backup ideas? Just send bullet points.", "Let's say", "If the page is refreshed, the new items disappear (are not saved to a database).", "We see when the page first loads and the user can add more.", "Please submit to Blackboard:", "The name of your app; A description of your app (what it does and who it's for);", "Your project features list (bullet points are fine) prioritized by &quot;Must Have&quot;, &quot;Should Have&quot; and &quot;Nice to Have&quot;.", "Thank you, looking forward to seeing your project!"];
+var item5=["PROJECT APPROVED", "Unfortunately, this is essentially a To Do list and, therefore, not permitted.", "What are your backup ideas? Just send bullet points.", "Let's say ", "If the page is refreshed, the new items disappear (are not saved to a database).", "We see when the page first loads and the user can add more.", "Please submit to Blackboard:", "The name of your app; A description of your app (what it does and who it's for);", "Your project features list (bullet points are fine) prioritized by &quot;Must Have&quot;, &quot;Should Have&quot; and &quot;Nice to Have&quot;.", "Thank you, looking forward to seeing your project!"];
 
 
 feedBack.push(item0, item1, item2, item3, item4, item5);
