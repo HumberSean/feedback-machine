e245b96ae38c41eb7b8c4fce8c997454436227ff
Sean W Doyle <sean.doyle@humber.ca>
2025-10-20 20:53:34 -0400

    Add WEBD100 Stage 1 feedback.

--- WD/WD-Stage-1.js
+++ WD/WD-Stage-1.js
@@ -1,9 +1,19 @@
+//WEBD100 Gated Assignment Stage 1: HTML Markup
+
+//
+
+
 var feedBack = [];
 
-const item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit."];
-const item1=["PART 1"];
-const item2=["PART 2"];
-const item3=["PART 3"];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "Please email me (or book a meeting) if you would like to go through this and have a strong foundation for the rest of the project."];
+var item1=["FILE NAMES", "Image files should be in lowercase with no spaces.", "All file names should be in lowercase with no spaces or special characters."];
+var item2=["IMAGES FOLDER", "You have added the folder, but not updated the src attributes to include the folder, which means your images aren't showing up."];
+var item3=["SITE NAVIGATION", "The top of every page should look the same on every page, with all menu items.", "Don't use target =&quot;_blank&quot; for internal site links. This opens up the page in a new tab. Use only for external links."];
+var item4=["LINKED LOGO", "Logo should be in top left corner."];
+var item5=["COMMENTING", "Use comments to mark the sections of your HTML file. This makes editing and maintenance easier."];
+var item6=["SEMANTIC USE OF ELEMENTS", "Text should not be on its own. Put it in a p element if it is a sentence.", "A sentence should be in a p element, not a heading.", "There can only be one h1 per page."];
+var item7=["STRUCTURAL ELEMENTS", "Missing a header element. This will hold the main title and logo and the navigation.", "Missing a footer element. This will have copyright info, extra nav if required, and social links."];
+var item8=["VALID MARKUP", "By 'trailing slash' it means elements that don't have a closing tag (meta, img), they shouldn't have a / at the closing &gt;.", "Use the W3C Validator to catch your errors. Fix these before starting Stage 2. Email me if any error message doesn't make sense."];
 
 
-feedBack.push(item0, item1, item2, item3);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8);
