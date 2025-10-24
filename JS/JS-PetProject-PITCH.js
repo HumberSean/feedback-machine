b089ad6bfd992f8066dacb81da6f9523d3532a4c
Sean W Doyle <sean.doyle@humber.ca>
2025-10-24 16:17:50 -0400

    Rearrage feedback.

--- JS/JS-PetProject-PITCH.js
+++ JS/JS-PetProject-PITCH.js
@@ -9,7 +9,7 @@ var item1=["MUST HAVE", "We specified ", "Let's say ", "+= ", "Move &quot;&quot;
 var item2=["SHOULD HAVE", "+= ", "Move &quot;&quot; to here."];
 var item3=["NICE TO HAVE", "+= ", "Move &quot;&quot; to here."];
 var item4=["NEXT STEPS", "Please proceed with your Figma mockup.", "Please revise based on feedback and resubmit."];
-var item5=["PROJECT APPROVED", "Unfortunately, this is essentially a To Do list and, therefore, not permitted.", "What are your backup ideas? Just send bullet points.", "Let's say ", "If the page is refreshed, the new items disappear (are not saved to a database).", "We see when the page first loads and the user can add more.", "Please submit to Blackboard:", "The name of your app; A description of your app (what it does and who it's for);", "Your project features list (bullet points are fine) prioritized by &quot;Must Have&quot;, &quot;Should Have&quot; and &quot;Nice to Have&quot;.", "Thank you, looking forward to seeing your project!"];
+var item5=["PROJECT APPROVED", "Unfortunately, this is essentially a To Do list and, therefore, not permitted.", "What are your backup ideas? Just send bullet points.", "Let's say ", "We see when the page first loads and the user can add more.", "If the page is refreshed, the new items disappear (are not saved to a database).", "Please submit to Blackboard:", "The name of your app; A description of your app (what it does and who it's for);", "Your project features list (bullet points are fine) prioritized by &quot;Must Have&quot;, &quot;Should Have&quot; and &quot;Nice to Have&quot;.", "Thank you, looking forward to seeing your project!"];
 
 
 feedBack.push(item0, item1, item2, item3, item4, item5);
