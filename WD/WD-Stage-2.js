64cac83c8289067ea76d0e923d24f4f427d1fb67
Sean W Doyle <sean.doyle@humber.ca>
2025-11-08 08:25:26 -0500

    Add WEBD1OO Stage 2 Feedback file

---
+++ WD/WD-Stage-2.js
@@ -0,0 +1,12 @@
+//WEBD100 Gated Assignment Stage 2: Layout & Styling
+
+//
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "Please email me (or book a meeting) if you would like to go through this and have a strong foundation for the rest of the project."];
+
+
+
+feedBack.push(item0);
