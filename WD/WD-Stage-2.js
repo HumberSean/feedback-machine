5195d8027f044d40fc189bce64a353cd7af5bb44
Sean W Doyle <sean.doyle@humber.ca>
2025-11-08 08:33:16 -0500

    Initial headings added.

--- WD/WD-Stage-2.js
+++ WD/WD-Stage-2.js
@@ -6,7 +6,10 @@
 var feedBack = [];
 
 var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "Please email me (or book a meeting) if you would like to go through this and have a strong foundation for the rest of the project."];
+var item1=["CSS FILE", "Use 1 CSS file for all pages to reduce number of files downloaded.", "Use comments to mark the sections of your CSS file. This makes editing and maintenance easier.", "Use the W3C Validator to catch your errors. Fix these before submitting Stage 3. Email me if any error message doesn't make sense."];
+var item2=["STYLING BLOCK ELEMENTS"];
+var item3=["NAVIGATION"];
+var item4=["FORM STYLING"];
 
 
-
-feedBack.push(item0);
+feedBack.push(item0, item1, item2, item3, item4);
