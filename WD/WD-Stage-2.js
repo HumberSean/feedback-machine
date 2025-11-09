16aec468d34768557c603f55fa1bd74a0e2b09e0
Sean W Doyle <sean.doyle@humber.ca>
2025-11-08 20:29:52 -0500

    Update feedback.

--- WD/WD-Stage-2.js
+++ WD/WD-Stage-2.js
@@ -6,10 +6,10 @@
 var feedBack = [];
 
 var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "Please email me (or book a meeting) if you would like to go through this and have a strong foundation for the rest of the project."];
-var item1=["CSS FILE", "Use 1 CSS file for all pages to reduce number of files downloaded.", "Use comments to mark the sections of your CSS file. This makes editing and maintenance easier.", "Use the W3C Validator to catch your errors. Fix these before submitting Stage 3. Email me if any error message doesn't make sense."];
-var item2=["STYLING BLOCK ELEMENTS"];
-var item3=["NAVIGATION"];
-var item4=["FORM STYLING"];
+var item1=["CSS FILE", "Use 1 CSS file for all pages to reduce number of files downloaded.", "Use comments to mark the sections of your CSS file. This makes editing and maintenance easier.", "Use the W3C Validator to catch your errors. Fix these before submitting Stage 3. Email me if any error message doesn't make sense.", "Excellent comments - very easy to navigate and make future changes!"];
+var item2=["STYLING BLOCK ELEMENTS", "Use flexbox for layout of multiple items.", "Use borders of block elements rather than the hr element.", "Flex needs to be applied to the containing block element, like a div or ol."];
+var item3=["NAVIGATION", "Navigation needs to be identical on every page. Copy and paste the header section into each web page."];
+var item4=["FORM STYLING", "Very nice styling!"];
 
 
 feedBack.push(item0, item1, item2, item3, item4);
