bd0d27cd352f1c6febd26c60db57031e7b98b517
Sean Doyle <sean.doyle@humber.ca>
2025-11-21 11:58:00 -0500

    Tweak feedback.

--- JS/JS-A-4-RegistrationForm.js
+++ JS/JS-A-4-RegistrationForm.js
@@ -1,16 +1,15 @@
 //JavaScript Assignment 4 - Form Handling
-//
+///
 
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Critical Fail: 0 for any changes made to the provided HTML file.", "Please add your name to your file name when submitting."];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Critical Fail: 0 for any changes made to the provided HTML file.", "Please add your name to your file name when submitting.", "Please pay careful attention to requirements."];
 var item1=["PART 1: SHOW AND HIDE", "Errors prevent form from submitting. Keep your console open to catch these.", "The header should disappear as well as the form (#1)."];
 var item2=["PART 2: OUTPUT", "Output should show full program name, not its short code (#2).", "Output is incorrect ().", "Output is incorrect. You did not use the provided p and span elements.", "They should NOT be in bold uppercase (#2.2). Please pay careful attention to requirements."];
 var item3=["PART 3: VALIDATION", "Background should be red, not the font (#3).", "Focus should be in the first box with an error.", "Validate one field at a time.", "Validate the fields in the order that the user sees them.", "Missing validation for the dropdown.", "Background is required to be 'red' (255, 0, 0). Please pay careful attention to requirements(#3).", "Don't hide the form until after you have validated all of the fields."];
-var item4=["STRETCH GOAL: CHECK IT WITH REGEX", "And the extra challenge too! Nice work!!", "Your regex allows for 9 digits. Use ^ and $ to limit a string.", "Your regex allows for 9 digits. Try 'N123456789' to see what I mean.", "Your regex does not allow for uppercase and lowercase N (#4)", "Your regex allows for +9 characters. Use ^ and $ to limit a string. Try 'xN123456789' to see what I mean."];
+var item4=["STRETCH GOAL: CHECK IT WITH REGEX", "And the extra challenge too! Nice work!!", "Your regex does not allow for uppercase and lowercase N (#4)", "Your regex allows for +9 characters. Use ^ and $ to limit a string. Try 'xN123456789' to see what I mean.", "Use a regular expression to check for the pattern."];
 var item5=["PROFESSIONAL PRACTICE", "Add helpful comments to mark the sections of your code and significant logic pieces. I encourage you to do this first (&quot;pseudocode&quot;) before you start your work.", "Missing responses to professional practice questions.", "Please email me if you are having trouble with this. We can make time to get you caught up. Understanding forms is critical for front-end web development."];
 
 
 feedBack.push(item0, item1, item2, item3, item4, item5);
-
