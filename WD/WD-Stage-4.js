0bbbf0f09e1e6b1865022f3b60e148cf458f023d
Sean Doyle <sean.doyle@humber.ca>
2025-11-22 13:11:20 -0500

    V1 feedback added.

--- WD/WD-Stage-4.js
+++ WD/WD-Stage-4.js
@@ -5,11 +5,16 @@
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "Please put all submission files in one folder to zip and submit.", "Please email me (or book a meeting) if you would like to go through this and have a strong foundation for the rest of the project."];
-var item1=["CSS FILE", "Use 1 CSS file for all pages to reduce number of files downloaded.", "Use comments to mark the sections of your CSS file. This makes editing and maintenance easier.", "Use the W3C Validator to catch your errors. Fix these before submitting Stage 3. Email me if any error message doesn't make sense.", "Excellent comments - very easy to navigate and make future changes!"];
-var item2=["STYLING BLOCK ELEMENTS", "Use flexbox for layout of multiple items.", "Use borders of block elements rather than the hr element.", "Flex needs to be applied to the containing block element, like a div or ol."];
-var item3=["NAVIGATION", "Navigation needs to be identical on every page. Copy and paste the header section into each web page."];
-var item4=["FORM STYLING", "Very nice styling!"];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Please put your name in your filename when you submit. Thank you!", "A great looking site - one for your portfolio!"];
+var item1=["FILE ORGANIZATION", "Use 1 CSS file for all pages to reduce number of files downloaded."];
+var item2=["VALIDATION", "Use the W3C Validator to catch your errors. Email me if any error message doesn't make sense."];
+var item3=["IMAGES", "Image files are much larger than the size at which they are being used.", "Images missing.", "Images missing alt attributes.", " image file is much larger than the size at which it is being used.", "No spaces or special characters in image file names."];
+var item4=["SITE STRUCTURE", "Order page is missing the footer."];
+var item5=["NAVIGATION", "Navigation needs to be identical on every page. Copy and paste the header section into each web page."];
+var item6=["SEMANTIC MARKUP", "Don't skip h levels.", "An article element &quot;...is designed for content that could be syndicated, like a blog post, news article, forum post, user-submitted comment, or even a product listing on an e-commerce site.&quot;", "Only one h1 on a page, subsequent heading should be sub-headings.", "Everything the user sees needs to be inside the body tags."];
+var item7=["FONT USAGE", "Font files not included correctly.", "Add backup fonts to font-family, ending with a generic font."];
+var item8=["FORM ELEMENTS", "Missing name attributes on input elements.", "Missing method=&quot;POST&quot; attribute on form tag.", "Fieldset element goes inside form element.", "You have duplicate name attributes for different inputs.", "Very nice styling!"];
+var item9=["CODING PRACTICES", "Use comments to mark the sections of your HTML and CSS file. This makes editing and maintenance easier.", "Use tabs to indent whenever an element is inside another element so that you can visually see what is inside what - especially div's.", "Excellent comments - very easy to navigate and make future changes!"];
 
 
-feedBack.push(item0, item1, item2, item3, item4);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9);
