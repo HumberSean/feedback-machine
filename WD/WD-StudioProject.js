1a0ce269f78827b5b40d99ce32df0c7c18e85466
Sean W Doyle <sean.doyle@humber.ca>
2025-12-16 17:39:06 -0500

    Add first round of feedback comments.

--- WD/WD-StudioProject.js
+++ WD/WD-StudioProject.js
@@ -1,20 +1,22 @@
-//WEBD100 Gated Assignment Stage 4: Final Project
+//WEBD100 Studio Project Website: Final Submission
 
 //
 
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "A great looking site - one for your portfolio!"];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "A great looking site - one for your portfolio!", "I would like to show this to future students. Please email me and let me know that I have your permission."];
 var item1=["FILE ORGANIZATION", "Use 1 CSS file for all pages to reduce number of files downloaded."];
-var item2=["VALIDATION", "Use the W3C Validator to catch your errors. Email me if any error message doesn't make sense."];
-var item3=["IMAGES", "Image files are much larger than the size at which they are being used.", "Images missing.", "Images missing alt attributes.", " image file is much larger than the size at which it is being used.", "No spaces or special characters in image file names."];
-var item4=["SITE STRUCTURE", "Order page is missing the footer."];
-var item5=["NAVIGATION", "Navigation needs to be identical on every page. Copy and paste the header section into each web page."];
-var item6=["SEMANTIC MARKUP", "Don't skip h levels.", "An article element &quot;...is designed for content that could be syndicated, like a blog post, news article, forum post, user-submitted comment, or even a product listing on an e-commerce site.&quot;", "Only one h1 on a page, subsequent heading should be sub-headings.", "Everything the user sees needs to be inside the body tags."];
+var item2=["VALIDATION", "Use the W3C Validator to catch your errors."];
+var item3=["IMAGES", "Image files are much larger than the size at which they are being used.", "Images missing.", "Images missing alt attributes.", " image file is much larger than the size at which it is being used.", "No spaces or special characters in image file names.", "Put keywords in your alt text.", "Put keywords in your image file names."];
+var item4=["SITE STRUCTURE", "Make tab text different for each page and include keywords."];
+var item5=["NAVIGATION", "Navigation needs to be identical on every page. Copy and paste the header section into each web page.", "Navigation items should be a ul with li elements."];
+var item6=["SEMANTIC MARKUP", "Don't skip h levels.", "An article element &quot;...is designed for content that could be syndicated, like a blog post, news article, forum post, user-submitted comment, or even a product listing on an e-commerce site.&quot;", "Only one h1 on a page, subsequent headings should be sub-headings.", "Everything the user sees needs to be inside the body tags."];
 var item7=["FONT USAGE", "Font files not included correctly.", "Add backup fonts to font-family, ending with a generic font."];
-var item8=["FORM ELEMENTS", "Missing name attributes on input elements.", "Missing method=&quot;POST&quot; attribute on form tag.", "Fieldset element goes inside form element.", "You have duplicate name attributes for different inputs.", "Very nice styling!"];
+var item8=["FORM ELEMENTS", "Missing name attributes on input elements.", "Missing method=&quot;POST&quot; attribute on form tag.", "Fieldset element goes inside form element.", "You have duplicate name attributes for different inputs.", "Your name attributes are mismatched with the expected input.", "Very nice styling!", "No form element used."];
 var item9=["CODING PRACTICES", "Use comments to mark the sections of your HTML and CSS file. This makes editing and maintenance easier.", "Use tabs to indent whenever an element is inside another element so that you can visually see what is inside what - especially div's.", "Excellent comments - very easy to navigate and make future changes!"];
+var item10=["CONTENT STRATEGY", "No Content Strategy document submitted.", "No updated Content Strategy document submitted with assignment."];
+var item11=["STYLING", "Logo does not match styling of content.", "Very plain styling, not engaging content other than two images."];
 
 
-feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11);
