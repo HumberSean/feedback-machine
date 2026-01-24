f4c62668f5d15f6d0b6f6fadc2b4d4d7f999a2bc
Sean W Doyle <sean.doyle@humber.ca>
2026-01-24 12:50:17 -0500

    Update feedback for 26W

--- SU/SU-lab-1-CodingStandards.js
+++ SU/SU-lab-1-CodingStandards.js
@@ -1,16 +1,14 @@
 ////Security Lab 1 Coding Standards
 
+//
+
+
 var feedBack = [];
 
+var item0=["FEEDBACK", "Great job!", "Good job!", "We will be using this lab in later classes so it is worthwhile to take the time and perfect it. You will have fewer issues later.", "We will be using this lab in later classes.", "Use comments to mark the sections of your code. I encourage you to do this before you start coding."];
+var item1=["PART 1", "For the strict BEM of this lab, use class attributes only (not id or name attributes).", "Does not follow BEM. Elements need 2 underscores: login__user", "Output value should be in its own span. Notice label is plain but value is bold.", "fieldset and legend go inside form element.", "You can add a second class to your inputs to provide a unique identifier for each. See what I mean in the output spans.", " indicates a modifier of . Add a second class with a new element value to add a unique identifier: ", "Styling is off. Pay close attention to provided mockup.", "Missing the heading.", "Missing legend text for fieldset."];
+var item2=["PART 2", "I was expecting the fields to turn red (see my CSS rule). We should not rely on the HTML validators.", "Required to add focus to error field.(#6)", "Focus should go to the first invalid field.", "This does not pass jslint.com"];
+var item3=["EXTRA CHALLENGE", "Nice job!", "Nice job adding the class!", "&quot;use strict&quot; has caused an error. Keep your console open and test everything after adding &quot;use strict&quot;."];
 
 
-item1 = ["FEEDBACK","Good job!", "We will be using this lab in later classes so it is worthwhile to take the time and perfect it. You will have fewer issues later.", "See rubric for more feedback."];
-
-item2 = ["PART 1", "Use BEM for ids and name attributes: login__user login__pass", "Missing the heading.", "Does not follow BEM. Elements need 2 underscores: output__text", "Output value should be in its own span. Notice label is plain but value is bold.", "fieldset and legend go inside form element."];
-
-
-item3 = ["PART 2", "I was expecting the fields to turn red (see my CSS rule). We should not rely on the HTML validators.", "This does not pass jslint.com", "Required to add focus to error field.(#6)"];
-
-item4 = ["EXTRA CHALLENGE", "Nice job!", "Nice job adding the class!"];
-
-feedBack.push(item1, item2, item3, item4);
+feedBack.push(item0, item1, item2, item3);
