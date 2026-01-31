d5c096dc63dd38cb7f05e2ea1e797617a81ad144
Sean Doyle <sean.doyle@humber.ca>
2026-01-31 14:06:09 -0500

    Rearrange feedback order.

--- SU/SU-Lab-3-UnitTesting-FEEDBACK.js
+++ SU/SU-Lab-3-UnitTesting-FEEDBACK.js
@@ -6,8 +6,8 @@ var feedBack = [];
 
 var item0=["FEEDBACK", "This was an in-class lab, but I will give you feedback anyway.", "Nice work!", "Nice work."];
 var item1=["PART 1: FUNCTION", "Function file should not get from or write to the web page.", "Use 1 regular expression to match the pattern of N or n with 8 digits.", "Declare all variables that the function needs inside the function."];
-var item2=["PART 2: BUILD TESTS", "Use += instead of = to append your results rather than overwrite them.", "Add a break tag to your markup so the results have line breaks.", "Check for actual return value of function, i.e. true/false.", "Unit test function should go in unit test file, not the function file.", "The unit test is not looking for true, it is looking to see if result === expected."];
-var item3=["PART 3: RUN TESTS", "Boundary tests for this would be N with 7 digits and N with 9 digits.", "Another option for boundary tests could be the values: n00000000 and n99999999.", " was already used as a test-to-pass.", " was already used as a test-to-fail.", "Test assertions will look for true, not the string 'true'."];
+var item2=["PART 2: BUILD TESTS", "Check for actual return value of function, i.e. true/false.", "Unit test function should go in unit test file, not the function file.", "The unit test is not looking for true, it is looking to see if result === expected.", "Use += instead of = to append your results rather than overwrite them.", "Add a break tag to your markup so that the results have line breaks."];
+var item3=["PART 3: RUN TESTS", "Test assertions will look for true, not the string 'true'.", "Your assertion does not match the logical expectation.  should return ", "Boundary tests for this would be N with 7 digits and N with 9 digits.", "Another option for boundary tests could be the values: n00000000 and n99999999.", " was already used as a test-to-pass.", " was already used as a test-to-fail."];
 var item4=["EXTRA CHALLENGE", "And the extra challenge too!", "You got the extra challenge!"];
 
 
