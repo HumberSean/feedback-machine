aacd3061026ab19570919bce33b5aff95405f89b
Sean Doyle <sean.doyle@humber.ca>
2026-02-16 09:41:02 -0500

    Add Security Hacker Prevention Assignment file.

---
+++ SU/SU-assign-2-HackerPrevention.js
@@ -0,0 +1,17 @@
+//
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+var item1=["PART 1"];
+var item2=["PART 2"];
+var item3=["SECURITY CONCERN"];
+var item4=["HACKER OBJECTIVES"];
+var item5=["NOTABLE CASES"];
+var item6=["HACK DEMO"];
+var item7=["PROTECTION STRATEGIES"];
+var item8=["DOCUMENT SUBMISSION"];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8);
