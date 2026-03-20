4adc2abde482acf351293401d61b0db30a71628e
Sean W Doyle <sean.doyle@humber.ca>
2026-03-20 11:23:40 -0400

    Initial feedback items. 26w

--- SU/SU-LAB-Test-Plan-Sketch.js
+++ SU/SU-LAB-Test-Plan-Sketch.js
@@ -1,11 +1,15 @@
 //USABILITY LAB - USABILITY TESTING SKETCH
+//
+
 
 var feedBack = [];
 
-const item0=["FEEDBACK", "Great job!", "Good job."];
-const item1=["PART 1"];
-const item2=["PART 2"];
-const item3=["PART 3"];
+var item0=["FEEDBACK", "Great job!", "Good job.", "No need to resubmit. Please use feedback for your Test Plan."];
+var item1=["OBJECTIVE", "Needs more specifics. What is", "We discussed", "You are describing functionality. What are you hoping to learn from someone trying to use your functionality?"];
+var item2=["SCOPE", "What will you not test?"];
+var item3=["EQUIPMENT", "Which browser? Which version #?", "Pick one browser. Save multi-browsers for compatability testing, not user testing."];
+var item4=["SCENARIOS", "Nice work on these!", "Provide input details for your test participant.", "Avoid being prescriptive in how to complete the task."];
+var item5=["METRICS", "Because you are deliberately forcing an error, you will want to know your user's response to the error.", "This is a case where time on task might be appropriate", "We discussed "];
 
 
-feedBack.push(item0, item1, item2, item3);
+feedBack.push(item0, item1, item2, item3, item4, item5);
