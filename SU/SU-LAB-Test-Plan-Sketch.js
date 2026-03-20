30cc31631a86fee39b6d056e79fd72fb07aa4536
Sean W Doyle <sean.doyle@humber.ca>
2026-03-20 10:26:12 -0400

    Create new feedback file for Usability Test Plan Sketch lab.

---
+++ SU/SU-LAB-Test-Plan-Sketch.js
@@ -0,0 +1,11 @@
+//USABILITY LAB - USABILITY TESTING SKETCH
+
+var feedBack = [];
+
+const item0=["FEEDBACK", "Great job!", "Good job."];
+const item1=["PART 1"];
+const item2=["PART 2"];
+const item3=["PART 3"];
+
+
+feedBack.push(item0, item1, item2, item3);
