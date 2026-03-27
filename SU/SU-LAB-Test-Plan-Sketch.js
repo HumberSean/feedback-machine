d9879be387e144f0e0acdd444cb054168d6ca543
Sean Doyle <sean.doyle@humber.ca>
2026-03-27 13:35:33 -0400

    Add feedback

--- SU/SU-LAB-Test-Plan-Sketch.js
+++ SU/SU-LAB-Test-Plan-Sketch.js
@@ -7,9 +7,9 @@ var feedBack = [];
 var item0=["FEEDBACK", "Great job!", "Good job.", "No need to resubmit. Please use feedback for your Test Plan."];
 var item1=["OBJECTIVE", "Needs more specifics. What is", "We discussed", "You are describing functionality. What are you hoping to learn from someone trying to use your functionality?"];
 var item2=["SCOPE", "What will you not test?"];
-var item3=["EQUIPMENT", "Which browser? Which version #?", "Pick one browser. Save multi-browsers for compatability testing, not user testing."];
-var item4=["SCENARIOS", "Nice work on these!", "Provide input details for your test participant.", "Avoid being prescriptive in how to complete the task."];
-var item5=["METRICS", "Because you are deliberately forcing an error, you will want to know your user's response to the error.", "This is a case where time on task might be appropriate", "We discussed "];
+var item3=["EQUIPMENT", "Which browser? Which version #?", "Pick one browser. Save multi-browsers for compatibility testing, not user testing.", "What is your rationale for risking the possibly confounding variable that different devices might introduce?"];
+var item4=["SCENARIOS", "Nice work on these!", "Provide input details for your test participant.", "Avoid being prescriptive in how to complete the task.", "Don't ask your participants to &quot;try to&quot;. Give them something they need to do."];
+var item5=["METRICS", "Because you are deliberately forcing an error, you will want to know your user's response to the error.", "This is a case where time on task might be appropriate", "We discussed"];
 
 
 feedBack.push(item0, item1, item2, item3, item4, item5);
