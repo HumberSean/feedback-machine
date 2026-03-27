4408a4facee93da8f51171480f8d2e55ca2174d8
Sean Doyle <sean.doyle@humber.ca>
2026-03-27 13:40:05 -0400

    Create U Test Plan Assignment feedback file.

---
+++ SU/SU-U-Assign-Test-Plan.js
@@ -0,0 +1,11 @@
+var feedBack = [];
+
+const item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback."];
+const item1=["OVERVIEW/SUMMARY"];
+const item2=["METHODOLOGY"];
+const item3=["TASKS"];
+const item4=["METRICS"];
+const item5=["SCRIPT &amp; SURVEYS"];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5);
