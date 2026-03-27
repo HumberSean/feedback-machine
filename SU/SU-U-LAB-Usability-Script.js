812b6bc95f279d84443b5f495eae8b155948fa8a
Sean Doyle <sean.doyle@humber.ca>
2026-03-27 14:57:09 -0400

    Initial feedback items.

--- SU/SU-U-LAB-Usability-Script.js
+++ SU/SU-U-LAB-Usability-Script.js
@@ -1,12 +1,16 @@
+//USABILITY LAB Krug Testing Script
+
+
 var feedBack = [];
 
-const item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback."];
-const item1=["INSTRUCTIONS"];
-const item2=["QUESTIONS"];
-const item3=["HOME PAGE TOUR"];
-const item4=["TASKS"];
-const item5=["PROBING"];
-const item5=["WRAPPING UP"];
+var item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback.", "Very well thought out.", "Good job."];
+var item1=["INSTRUCTIONS", "Sometimes people are afraid to give honest feedback because they don't want to hurt people's feelings. If we let a test participant know that they don't need to worry about hurting our feelings, it gives them the &quot;permission&quot; that they may not have felt they had.", "Are you going to answer their questions when they ask them? If not, let them know in advance that you might not.", "How long will this session take? Let your participant know.", "Don't tell them how to use the app."];
+var item2=["QUESTIONS"];
+var item3=["HOME PAGE TOUR"];
+var item4=["TASKS"];
+var item5=["PROBING", "If you are going to ask them before the test if they use something with similar functionality, ask them afterward how it compares to what they have used."];
+var item6=["WRAPPING UP"];
+var item7=["CODING CHALLENGE", "window.onload can only happen once, so create your script so that you could add it at the bottom of the page you are testing without needing onload.", "Create code that could be pasted into the bottom of the page that you want to test."];
 
 
-feedBack.push(item0, item1, item2, item3, item4, item5);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
