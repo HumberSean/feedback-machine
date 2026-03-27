f7d539e92317910b91c9b6d36f9b2c6943a377cd
Sean Doyle <sean.doyle@humber.ca>
2026-03-27 13:54:28 -0400

    Create A lab Testing Script feedback file.

---
+++ SU/SU-U-LAB-Usability-Script.js
@@ -0,0 +1,12 @@
+var feedBack = [];
+
+const item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback."];
+const item1=["INSTRUCTIONS"];
+const item2=["QUESTIONS"];
+const item3=["HOME PAGE TOUR"];
+const item4=["TASKS"];
+const item5=["PROBING"];
+const item5=["WRAPPING UP"];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5);
