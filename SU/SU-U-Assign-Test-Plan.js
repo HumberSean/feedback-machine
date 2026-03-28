aec3798aa9dd4191fb9b30f3618474a2a1ea1744
Sean Doyle <sean.doyle@humber.ca>
2026-03-28 11:49:35 -0400

    Add initial feedback.

--- SU/SU-U-Assign-Test-Plan.js
+++ SU/SU-U-Assign-Test-Plan.js
@@ -1,11 +1,16 @@
+//USABILITY ASSIGNMENT - TESTING PLAN
+//
+
+
 var feedBack = [];
 
-const item0=["FEEDBACK", "Great job!", "Good job. See rubric for more feedback."];
-const item1=["OVERVIEW/SUMMARY"];
-const item2=["METHODOLOGY"];
-const item3=["TASKS"];
-const item4=["METRICS"];
-const item5=["SCRIPT &amp; SURVEYS"];
+var item0=["FEEDBACK", "Great job!", "Good job.", "Nice attention to detail.", "Please incorporate this feedback into your final report submission.", "Very well thought out."];
+var item1=["OVERVIEW/SUMMARY", "What will you exclude from testing?"];
+var item2=["METHODOLOGY", "What specific hardware/software will you be using to run your tests?", "Participants: very nice specifics!", "Training: Be careful not to give too much away on how the app works before testing."];
+var item3=["TASKS", "Scenario feedback was given in the last class/lab. Please adjust your final report document to include this feedback and your actual test scenarios."];
+var item4=["METRICS"];
+var item5=["SCRIPT & SURVEYS"];
+var item6=["USABILITY GOALS", "What are your goals for time on task?", "What are your goals for subjective measures?"];
 
 
-feedBack.push(item0, item1, item2, item3, item4, item5);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6);
