ed97e320b9875efe02df1d8689f4e0d546cc46a8
Sean Doyle <sean.doyle@humber.ca>
2026-03-29 11:01:07 -0400

    Add initial feedback.

---
+++ SU/SU-U-Assign-Heuristic-Team.js
@@ -0,0 +1,20 @@
+//Usability Heuristic Eval Group Report
+//
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Good job, team.", "Some excellent observations.", "Some great recommendations!", "Great work, team! Very specific observations, and clear, actionable recommendations.", "Please send this feedback to your other team members. Since you submitted on behalf of your team, you are the only one that will receive this feedback."];
+var item1=["VISIBILITY OF SYSTEM STATUS", "What are your recommendations?"];
+var item2=["MATCH BETWEEN SYSTEM AND THE REAL WORLD", "What are your recommendations?"];
+var item3=["USER CONTROL AND FREEDOM", "What are your recommendations?", "Why not 10 out of 10?"];
+var item4=["CONSISTENCY AND STANDARDS", "What are your recommendations?", "Why not 10 out of 10?"];
+var item5=["ERROR PREVENTION", "What are your recommendations?", "Why not 10 out of 10?"];
+var item6=["RECOGNITION RATHER THAN RECALL", "What are your recommendations?", "Why not 10 out of 10?"];
+var item7=["FLEXIBILITY AND EFFICIENCY OF USE.", "What are your recommendations?", "Why not 10 out of 10?", "What did they do well that rated a ?"];
+var item8=["AESTHETIC AND MINIMALIST DESIGN", "What are your recommendations?", "Why not 10 out of 10?", "What did they do well that rated a ?"];
+var item9=["HELP USERS RECOGNIZE, DIAGNOSE, AND RECOVER FROM ERRORS", "What are your recommendations?", "What are your recommendations?", "Why not 10 out of 10?", "What did they do well that rated a ?"];
+var item10=["HELP AND DOCUMENTATION", "There is a menu FAQ page (found in the footer nav)", "What are your recommendations?", "Why not 10 out of 10?", "What did they do well that rated a ?"];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
