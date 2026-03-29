627ffb3eb97a58f9ee509cd78b55bbb10c91128c
Sean Doyle <sean.doyle@humber.ca>
2026-03-29 09:49:27 -0400

    Create Usability Heuristic Eval Lab feedback.

---
+++ SU/SU-LAB-HeuristicEval-Individual.js
@@ -0,0 +1,19 @@
+//Usability Heuristic Eval-Individual
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Great job!", "Good job.", "Some excellent observations.", "Some great recommendations!"];
+var item1=["VISIBILITY OF SYSTEM STATUS"];
+var item2=["MATCH BETWEEN SYSTEM AND THE REAL WORLD"];
+var item3=["USER CONTROL AND FREEDOM"];
+var item4=["CONSISTENCY AND STANDARDS"];
+var item5=["ERROR PREVENTION"];
+var item6=["RECOGNITION RATHER THAN RECALL"];
+var item7=["FLEXIBILITY AND EFFICIENCY OF USE.", "Sign in for faster checkout."];
+var item8=["AESTHETIC AND MINIMALIST DESIGN"];
+var item9=["HELP USERS RECOGNIZE, DIAGNOSE, AND RECOVER FROM ERRORS"];
+var item10=["HELP AND DOCUMENTATION", "FAQ in footer."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
