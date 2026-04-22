dfa8cfc1b971351b11889b4f196f0f77829d9309
Sean Doyle <sean.doyle@humber.ca>
2026-04-22 10:41:26 -0400

    Create feedback file for Usability Report.

---
+++ SU/SU-U_Usability-Report.js
@@ -0,0 +1,15 @@
+//Usability Report
+
+
+var feedBack = [];
+
+var item0=["PRESENTATION", "Great use of slides!"];
+var item1=["PRESENTATION: OVERVIEW", "What is the app and who is it for? Make this clear off the top and tie into it through your presentation."];
+var item2=["PRESENTATION: OBJECTIVES", "What were you hoping to achieve with your testing?"];
+var item3=["PRESENTATION: SCENARIO STRATEGY", "How do your scenarios support your objectives?", "What is the rationale for your specified scenarios?"];
+var item4=["PRESENTATION: DATA SUMMARY"];
+var item5=["PRESENTATION: RECOMMENDATIONS", "Prioritize your recommendations. Make clear what are the most important next steps. Bulleted lists do not convey this on their own."];
+
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5);
