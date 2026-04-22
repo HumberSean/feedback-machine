5ab58b2ee51561e988f6a72e336cace1ba62c40e
Sean Doyle <sean.doyle@humber.ca>
2026-04-22 11:54:53 -0400

    Start Usability Report: Document feedback headings.

--- SU/SU-U_Usability-Report.js
+++ SU/SU-U_Usability-Report.js
@@ -1,15 +1,23 @@
-//Usability Report
+//Usability Report: Presentation & Document
+
+//
 
 
 var feedBack = [];
-
+var itemx=["Feedback"];
 var item0=["PRESENTATION", "Great use of slides!"];
 var item1=["PRESENTATION: OVERVIEW", "What is the app and who is it for? Make this clear off the top and tie into it through your presentation."];
 var item2=["PRESENTATION: OBJECTIVES", "What were you hoping to achieve with your testing?"];
 var item3=["PRESENTATION: SCENARIO STRATEGY", "How do your scenarios support your objectives?", "What is the rationale for your specified scenarios?"];
 var item4=["PRESENTATION: DATA SUMMARY"];
 var item5=["PRESENTATION: RECOMMENDATIONS", "Prioritize your recommendations. Make clear what are the most important next steps. Bulleted lists do not convey this on their own."];
+var item6=["DOCUMENT"];
+var item7=["DOCUMENT: SUMMARY OF TESTING"];
+var item8=["DOCUMENT: SUMMARY OF RESULTS"];
+var item9=["DOCUMENT: SIGNIFICANT DATA"];
+var item10=["DOCUMENT: RECOMMENDATIONS"];
+var item11=["DOCUMENT: FINAL SUMMARY"];
+var item12=["DOCUMENT: APPENDIX"];
 
 
-
-feedBack.push(item0, item1, item2, item3, item4, item5);
+feedBack.push(itemx, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12);
