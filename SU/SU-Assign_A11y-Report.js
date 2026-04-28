f3a49a9fb540d8128a18ee4e588202d154b198c3
Sean W Doyle <sean.doyle@humber.ca>
2026-04-28 19:10:05 -0400

    Create Accessibility Report feedback file with initial feedback.

---
+++ SU/SU-Assign_A11y-Report.js
@@ -0,0 +1,15 @@
+//A11y Report
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Some terrific observations and analysis!", "Nice work here!", "Good job."];
+var item1=["WAVE ASSESSMENT"];
+var item2=["SEMANTIC ASSESSMENT", "Are you using the form elements for your quiz?"];
+var item3=["IMAGE ASSESSMENT"];
+var item4=["ARIA ASSESSMENT", "If there are currently none, what could there be?"];
+var item5=["KEYBOARD ACCESSIBILITY ASSESSMENT", "Can you complete your testing scenarios using only your keyboard?"];
+var item6=["RECOMMENDATIONS", "You might want to move #6 recommendation higher on the list since semantic elements are our starting point for accessibility.", "Semantic elements should be a higher priority since semantic elements are our starting point for accessibility."];
+
+
+feedBack.push(item0, item1, item2, item3, item4, item5, item6);
