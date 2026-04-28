a2d515197740c4080c7593483ba75ac34832a45e
Sean W Doyle <sean.doyle@humber.ca>
2026-04-28 18:07:35 -0400

    Add initial report document feedback.

--- SU/SU-U_Usability-Report.js
+++ SU/SU-U_Usability-Report.js
@@ -4,20 +4,21 @@
 
 
 var feedBack = [];
-var itemx=["Feedback"];
-var item0=["PRESENTATION", "Great use of slides!"];
-var item1=["PRESENTATION: OVERVIEW", "What is the app and who is it for? Make this clear off the top and tie into it through your presentation."];
-var item2=["PRESENTATION: OBJECTIVES", "What were you hoping to achieve with your testing?"];
-var item3=["PRESENTATION: SCENARIO STRATEGY", "How do your scenarios support your objectives?", "What is the rationale for your specified scenarios?"];
-var item4=["PRESENTATION: DATA SUMMARY"];
-var item5=["PRESENTATION: RECOMMENDATIONS", "Prioritize your recommendations. Make clear what are the most important next steps. Bulleted lists do not convey this on their own."];
-var item6=["DOCUMENT"];
-var item7=["DOCUMENT: SUMMARY OF TESTING"];
-var item8=["DOCUMENT: SUMMARY OF RESULTS"];
-var item9=["DOCUMENT: SIGNIFICANT DATA"];
-var item10=["DOCUMENT: RECOMMENDATIONS"];
-var item11=["DOCUMENT: FINAL SUMMARY"];
-var item12=["DOCUMENT: APPENDIX"];
+
+var item0=["FEEDBACK"];
+var item1=["PRESENTATION", "Great use of slides!"];
+var item2=["PRESENTATION: OVERVIEW", "What is the app and who is it for? Make this clear off the top and tie into it through your presentation."];
+var item3=["PRESENTATION: OBJECTIVES", "What were you hoping to achieve with your testing?"];
+var item4=["PRESENTATION: SCENARIO STRATEGY", "How do your scenarios support your objectives?", "What is the rationale for your specified scenarios?"];
+var item5=["PRESENTATION: DATA SUMMARY"];
+var item6=["PRESENTATION: RECOMMENDATIONS", "Prioritize your recommendations. Make clear what are the most important next steps. Bulleted lists do not convey this on their own."];
+var item7=["DOCUMENT", "Very nice work."];
+var item8=["DOCUMENT: SUMMARY OF TESTING"];
+var item9=["DOCUMENT: SUMMARY OF RESULTS", "Can we see some charted data? This will support your summary and recommendations."];
+var item10=["DOCUMENT: SIGNIFICANT DATA", "I appreciate that you identified what was significant about each observation.", "What is significant about this?"];
+var item11=["DOCUMENT: RECOMMENDATIONS", "If the results are mixed, it is perfectly legitimate to recommend further testing.", "Prioritize your recommendations. Make clear what are the most important next steps. Bulleted lists do not convey this on their own", "As above in your presentation feedback, prioritize your recommendations. Your test plan document has specified severity classifications but you have not applied them.", "You have provided what your participants recommend. What do you recommend?"];
+var item12=["DOCUMENT: FINAL SUMMARY", "Missing final summary.What are your final thoughts on what you experienced and learned from your user testing?"];
+var item13=["DOCUMENT: APPENDIX", "Missing print out of scenario tasks.", "Missing testing script and scenario tasks print out.", "This should all be in one document."];
 
 
-feedBack.push(itemx, item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10, item11, item12, item13);
