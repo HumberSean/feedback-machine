09862908683429c9bc08409e1f64c63c15cb0922
Sean W Doyle <sean.doyle@humber.ca>
2026-05-14 07:41:56 -0400

    Add Capstone Figma Assignment feedback.

---
+++ CP/CP-Figma.js
@@ -0,0 +1,13 @@
+//CAPSTONE-Figma User Testing Report
+
+
+var feedBack = [];
+
+var item0=["INTRODUCTION", "It is worth noting that you are testing the prototype, not the actual app.", "Missing an intro to explain your testing scope and objectives."];
+var item1=["TASKS", "Give specific tasks. This keeps your testing consistent.", "Provide necessary info to complete the task.", "Where is your data? How do you know you have achieved &quot;success&quot;?"];
+var item2=["CONCLUSION AND RECOMMENDATIONS", "More testing is needed. Do the full 5 recommended by Krug.", "What are your priorities for fixes - what will you fix first and why?", "How will you implement the recommendations?"];
+
+
+
+
+feedBack.push(item0, item1, item2);
