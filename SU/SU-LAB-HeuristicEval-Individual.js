78bef055998858c13cd3fab256631e46347011e7
Sean Doyle <sean.doyle@humber.ca>
2026-05-15 14:15:46 -0400

    Add 26S feedback.

--- SU/SU-LAB-HeuristicEval-Individual.js
+++ SU/SU-LAB-HeuristicEval-Individual.js
@@ -1,19 +1,21 @@
 //Usability Heuristic Eval-Individual
 
+//
+
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Great job!", "Good job.", "Some excellent observations.", "Some great recommendations!"];
-var item1=["VISIBILITY OF SYSTEM STATUS"];
+var item0=["FEEDBACK", "Great job!", "Good job.", "Some excellent observations.", "Some great recommendations!", "Some excellent observations and recommendations!", "It seems as though you were assessing the website generally, rather than using the heuristics as your benchmarks."];
+var item1=["VISIBILITY OF SYSTEM STATUS", "What are your recommendations to address this?", "What about the general status of the pizza ordering process?"];
 var item2=["MATCH BETWEEN SYSTEM AND THE REAL WORLD"];
 var item3=["USER CONTROL AND FREEDOM"];
 var item4=["CONSISTENCY AND STANDARDS"];
-var item5=["ERROR PREVENTION"];
-var item6=["RECOGNITION RATHER THAN RECALL"];
-var item7=["FLEXIBILITY AND EFFICIENCY OF USE.", "Sign in for faster checkout."];
-var item8=["AESTHETIC AND MINIMALIST DESIGN"];
-var item9=["HELP USERS RECOGNIZE, DIAGNOSE, AND RECOVER FROM ERRORS"];
-var item10=["HELP AND DOCUMENTATION", "FAQ in footer."];
+var item5=["ERROR PREVENTION", "What about preventing the error before it happens?", "How should it prevent this error?"];
+var item6=["RECOGNITION RATHER THAN RECALL", "Does the design keep important information visible, so that users do not have to memorize it?"];
+var item7=["FLEXIBILITY AND EFFICIENCY OF USE.", "Sign in for faster checkout.", "You can save previous orders."];
+var item8=["AESTHETIC AND MINIMALIST DESIGN", "Is the visual design and content focused on the essentials?"];
+var item9=["HELP USERS RECOGNIZE, DIAGNOSE, AND RECOVER FROM ERRORS", "What are your recommendations to address this?"];
+var item10=["HELP AND DOCUMENTATION", "FAQ in footer.", "Where would a user go if they needed help?"];
 
 
 feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7, item8, item9, item10);
