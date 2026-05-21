55cf3483fcd5722cd789b100c7db67cf0778e1de
Sean W Doyle <sean.doyle@humber.ca>
2026-05-21 10:51:03 -0400

    Update initial feedback.

--- CP/CP-Estimates.js
+++ CP/CP-Estimates.js
@@ -1,11 +1,13 @@
 //Capstone Project Estimates
 
+//
+
 
 var feedBack = [];
 
-var item0=["FEEDBACK"];
-var item1=["TIMELINE", "Needs more detail.", "Difficulty: evaluate the difficulty of each task", "Due Date: set a target date for each task", "Missing time to create the interface designs.", "Missing time to plan, execute and report on user testing.", "User testing will be in Week 4.", "Consider leaving authentication until after you have something working.", "Update to reflect changes made to requirements document.", "Don't leave deployment to Week 7 as there are often surprises that occur when moving from local to the internet.", "This document is not your time sheet (which tracks your time). The timeline is your schedule of what you plan to accomplish each week. Please complete and resubmit.", "Missing deployment. Do not leave this to the last week as there are often surprises that occur when moving from local to the internet.", "Missing user testing of Figma prototype."];
-var item2=["TIME ESTIMATE AND RATE"];
+var item0=["FEEDBACK", "Nice work on this, very well considered."];
+var item1=["TIMELINE", "Needs more detail.", "Difficulty: evaluate the difficulty of each task", "Due Date: set a target date for each task", "Missing time to create the interface designs.", "Missing time to plan, execute and report on user testing.", "User testing will be in Week 4.", "Consider leaving authentication until after you have something working.", "Update to reflect changes made to requirements document.", "Don't leave deployment to Week 7 as there are often surprises that occur when moving from local to the internet.", "Missing deployment. Do not leave this to the last week as there are often surprises that occur when moving from local to the internet.", "Missing user testing of Figma prototype.", "Missing presentation time estimation.", "Missing time to plan and design your usability testing."];
+var item2=["TIME ESTIMATE AND RATE", " hours is very high for 7 weeks of work. Are you expecting to put in this time?", " hours is on the low side for a Capstone project."];
 
 
 feedBack.push(item0, item1, item2);
