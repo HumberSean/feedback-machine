79bc71f44ada6f3ecd246744c5376692c520c8db
Sean W Doyle <sean.doyle@humber.ca>
2026-05-21 10:20:35 -0400

    Take timeline feedback from req doc feedback and create new Estimates feedback file.

---
+++ CP/CP-Estimates.js
@@ -0,0 +1,11 @@
+//Capstone Project Estimates
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK"];
+var item1=["TIMELINE", "Needs more detail.", "Difficulty: evaluate the difficulty of each task", "Due Date: set a target date for each task", "Missing time to create the interface designs.", "Missing time to plan, execute and report on user testing.", "User testing will be in Week 4.", "Consider leaving authentication until after you have something working.", "Update to reflect changes made to requirements document.", "Don't leave deployment to Week 7 as there are often surprises that occur when moving from local to the internet.", "This document is not your time sheet (which tracks your time). The timeline is your schedule of what you plan to accomplish each week. Please complete and resubmit.", "Missing deployment. Do not leave this to the last week as there are often surprises that occur when moving from local to the internet.", "Missing user testing of Figma prototype."];
+var item2=["TIME ESTIMATE AND RATE"];
+
+
+feedBack.push(item0, item1, item2);
