001c4378a10b02d018b62527694cd5ce5fc2f365
Sean W Doyle <sean.doyle@humber.ca>
2026-06-02 21:18:42 -0400

    Add 26S feedback.

--- SU/SU-U-Assign_Test-Plan.js
+++ SU/SU-U-Assign_Test-Plan.js
@@ -1,16 +1,18 @@
 //USABILITY ASSIGNMENT - TESTING PLAN
 //
+//
 
 
 var feedBack = [];
 
 var item0=["FEEDBACK", "Great job!", "Good job.", "Nice attention to detail.", "Please incorporate this feedback into your final report submission.", "Very well thought out."];
 var item1=["OVERVIEW/SUMMARY", "What will you exclude from testing?"];
-var item2=["METHODOLOGY", "What specific hardware/software will you be using to run your tests?", "Participants: very nice specifics!", "Training: Be careful not to give too much away on how the app works before testing."];
-var item3=["TASKS", "Scenario feedback was given in the last class/lab. Please adjust your final report document to include this feedback and your actual test scenarios."];
+var item2=["METHODOLOGY", "What specific hardware/software will you be using to run your tests?", "Participants: very nice specifics!", "Training: Be careful not to give too much away on how the app works before testing.", "If you are not using 5 participants (as specified by Krug) you need to justify your choice in this document.", "Will your sessions be &quot;videotaped&quot;?"];
+var item3=["TASKS", "Scenario feedback was given in the last class/lab. Please adjust your final report document to include this feedback and your actual test scenarios.", "Don't ask them to &quot;try to&quot; do something, give them the task."];
 var item4=["METRICS"];
 var item5=["SCRIPT & SURVEYS"];
 var item6=["USABILITY GOALS", "What are your goals for time on task?", "What are your goals for subjective measures?"];
+var item7=["PROBLEM SEVERITY CLASSIFICATION", "If you are using these default classifications, I will be looking for them in your final report."];
 
 
-feedBack.push(item0, item1, item2, item3, item4, item5, item6);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
