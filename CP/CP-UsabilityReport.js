69015a2d9eadf441d0ac73e63e56dc0c61795d4a
Sean W Doyle <sean.doyle@humber.ca>
2026-06-15 18:40:32 -0400

    Add 26S feedback

--- CP/CP-UsabilityReport.js
+++ CP/CP-UsabilityReport.js
@@ -7,9 +7,11 @@ var feedBack = [];
 
 var item0=["FEEDBACK", "Very nice work!", "You found target users - excellent!!"];
 var item1=["REPORT DOCUMENT", "Document is missing sections. Use the template provided in the Usability course.", "Very well organized."];
-var item2=["SCENARIOS/TASKS", "Missing admin-side task.", "Missing public-side task.", "Provide specific values for your user to enter during their tasks.", "Provide details of your scenarios/tasks.", "Provide your scenarios/tasks in this document.", "Nice detail in your tasks!"];
-var item3=["FINDINGS", "Missing details of interactions.", "You have provided the individual data, but we need your review of the overall aggregate data. E.g. &quot;2 out of 5 people had difficulty updating the product.&quot;,&quot;5 of 5 completed the task successfully.&quot;", "Don't decide what they thought, what did you see/hear?"];
-var item4=["ASSESSMENT/SUMMARY", "Very astute assessment.", "Missing your overall assessment and recommendations.", "What specific actions will you take to address the identified issues?", "Prioritize your results. What is urgent or most important?"];
+var item2=["SCENARIOS/TASKS", "Missing admin-side task.", "Missing public-side task.", "Provide specific values for your user to enter during their tasks.", "Provide details of your scenarios/tasks.", "Nice detail in your tasks!", "Provide your scenarios/tasks in this document. Attach as an appendix."];
+var item3=["USABILITY GOALS", "What were your goals for Time on Task?", "What were your goals for Subjective Measures?", "What were your goals for Time on Task and Subjective Measures?"];
+var item4=["FINDINGS", "Missing details of interactions.", "You have provided the individual data, but we need your review of the overall aggregate data. E.g. &quot;2 out of 5 people had difficulty updating the product.&quot;,&quot;5 of 5 completed the task successfully.&quot;", "Don't decide what they thought, what did you see/hear?", "Clear reporting of collected data."];
+var item5=["ASSESSMENT/SUMMARY", "Very astute assessment.", "Missing your overall assessment and recommendations.", "What specific actions will you take to address the identified issues?", "Prioritize your results. What is urgent or most important?"];
 
 
-feedBack.push(item0, item1, item2, item3, item4);
+
+feedBack.push(item0, item1, item2, item3, item4, item5);
