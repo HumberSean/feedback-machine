05061f0424a8def10e72c8f2221d27050dd4d10b
Sean Doyle <sean.doyle@humber.ca>
2026-06-29 14:12:27 -0400

    Update feedback with final results.

--- CP/CP-Estimates.js
+++ CP/CP-Estimates.js
@@ -8,6 +8,7 @@ var feedBack = [];
 var item0=["FEEDBACK", "Nice work on this, very well considered."];
 var item1=["TIMELINE", "Needs more detail.", "Difficulty: evaluate the difficulty of each task", "Due Date: set a target date for each task", "Missing time to create the interface designs.", "Missing time to plan, execute and report on user testing.", "User testing will be in Week 4.", "Consider leaving authentication until after you have something working.", "Update to reflect changes made to requirements document.", "Don't leave deployment to Week 7 as there are often surprises that occur when moving from local to the internet.", "Missing deployment. Do not leave this to the last week as there are often surprises that occur when moving from local to the internet.", "Missing user testing of Figma prototype.", "Missing presentation time estimation.", "Missing time to plan and design your usability testing."];
 var item2=["TIME ESTIMATE AND RATE", " hours is very high for 7 weeks of work. Are you expecting to put in this time?", " hours is on the low side for a Capstone project."];
+var item3=["PROJECT FINAL NUMBERS", "Budget: $", "Spent: $", "Difference: $3,320.00 under over budget.", "At almost $10000 off, it means you either didn't invoice for some of the time that you spent, or you grossly overestimated the time it would take and, therefore, would have risked losing the work by being under-bid by someone else."];
 
 
-feedBack.push(item0, item1, item2);
+feedBack.push(item0, item1, item2, item3);
