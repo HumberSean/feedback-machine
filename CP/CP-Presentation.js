e73cf44e932b9a7997e1996bdd2a722d54f001d5
Sean Doyle <sean.doyle@humber.ca>
2026-06-29 14:43:58 -0400

    Update feedback 26s

--- CP/CP-Presentation.js
+++ CP/CP-Presentation.js
@@ -3,14 +3,15 @@
 
 
 var feedBack = [];
+var item0=["FEEDBACK", "Too long. Stay within allowed time to keep listeners listening. What could you have gotten rid of or tightened up?"];
+var item1=["PROJECT DEPLOYMENT", "Not deployed for presentation."];
+var item2=["PROJECT PURPOSE", "Who is this site for?", "A technical description, but missing the human problem it's solving."];
+var item3=["TECHNOLOGY USED", "What is your tech stack?"];
+var item4=["FEATURE LEVEL ACHIEVED", "How much of your original plan did you achieve? Must Have; Should Have; Nice to Have?"];
+var item5=["DEMO OF MAIN FEATURES", "Missing", "Don't slow your demo down with logins. Have a browser ready and logged in for every role.", "Use real values (not blahblahblah) for input values. This provides clearer context for the viewer."];
+var item6=["UX TESTING INSIGHTS", "What did you learn, what changes did you make based on your user testing?"];
+var item7=["NEXT STEPS", "What are the future plans for this app?"];
 
-var item0=["PROJECT DEPLOYMENT", "Not deployed for presentation."];
-var item1=["PROJECT PURPOSE", "Who is this site for?", "A technical description, but missing the human problem it's solving."];
-var item2=["TECHNOLOGY USED", "What is your tech stack?"];
-var item3=["FEATURE LEVEL ACHIEVED", "How much of your original plan did you achieve? Must Have; Should Have; Nice to Have?"];
-var item4=["DEMO OF MAIN FEATURES", "Missing"];
-var item5=["UX TESTING INSIGHTS", "What did you learn, what changes did you make based on your user testing?"];
-var item6=["NEXT STEPS", "What are the future plans for this app?"];
 
 
-feedBack.push(item0, item1, item2, item3, item4, item5, item6);
+feedBack.push(item0, item1, item2, item3, item4, item5, item6, item7);
