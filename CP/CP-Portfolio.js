a96a6ac71ecb2e2da5f1bc8c4a3d65845f78f27d
Sean Doyle <sean.doyle@humber.ca>
2026-06-29 13:43:11 -0400

    Fix initial feedback.

--- CP/CP-Portfolio.js
+++ CP/CP-Portfolio.js
@@ -3,7 +3,7 @@
 
 var feedBack = [];
 
-var item0=["FEEDBACK", "Replace the WP favicon with your own favicon.", "Create a favicon for your site. If no log, you can use your initials."];
+var item0=["FEEDBACK", "Replace the WP favicon with your own favicon.", "Create a favicon for your site. If you don't have a logo, you can use your initials."];
 var item1=["PROJECTS", "Have your project links open in a new tab (target=_blank) so that your portfolio stays in its own tab.", "Include screenshots in your project overview.", "Provide some context for your items. Explain your thought/creative/dev processes to showcase these skills."];
 
 
