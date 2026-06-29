81a602a2af7e6549388288caa0b4f6a2d58ad917
Sean Doyle <sean.doyle@humber.ca>
2026-06-29 10:34:38 -0400

    Add Portfolio Feedback file.

---
+++ CP/CP-Portfolio.js
@@ -0,0 +1,10 @@
+//Capstone Portfolio Feedback
+
+
+var feedBack = [];
+
+var item0=["FEEDBACK", "Replace the WP favicon with your own favicon.", "Create a favicon for your site. If no log, you can use your initials."];
+var item1=["PROJECTS", "Have your project links open in a new tab (target=_blank) so that your portfolio stays in its own tab.", "Include screenshots in your project overview.", "Provide some context for your items. Explain your thought/creative/dev processes to showcase these skills."];
+
+
+feedBack.push(item0, item1);
